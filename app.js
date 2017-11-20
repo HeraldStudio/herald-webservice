8295371186969f5486b2c984ad894b0803a04552
vhyme <vhyme@live.cn>
2017-11-20 17:36:30 +0800

    首次提交

---
+++ app.js
@@ -0,0 +1,31 @@
+const koa = require('koa')
+const route = require('koa-route')
+const cache = require('./cache')
+const app = new koa()
+const sleep = t => new Promise(r => setTimeout(r, t))
+
+app.get     = (r, f) => app.use(route.get     (r, f))
+app.post    = (r, f) => app.use(route.post    (r, f))
+app.put     = (r, f) => app.use(route.put     (r, f))
+app.delete  = (r, f) => app.use(route.delete  (r, f))
+app.options = (r, f) => app.use(route.options (r, f))
+
+app.use(async (ctx, next) => {
+  ctx.state.meta = JSON.stringify([ ctx.url, ctx.method, ctx.headers.token, ctx.query ])
+  console.log(ctx.state.meta)
+  let cached = cache.get(ctx.state.meta)
+  if (cached) {
+    ctx.body = cached
+  } else {
+    await next()
+    cache.set(ctx.state.meta, ctx.body, ctx.state.ttl)
+  }
+})
+
+app.get('/', async ctx => {
+  await sleep(3000)
+  ctx.body = { hello: 'world' }
+  ctx.state.ttl = 10
+})
+
+app.listen(3000)
