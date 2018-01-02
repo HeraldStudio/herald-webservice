8075053014f6aed61fbf8d540cc25f8d10fc5c54
Vigilans <vigilans@foxmail.com>
2018-01-02 21:13:20 +0800

    开始添加空教室功能
    目前实现了将教一~教八的空教室查询请求转发至学校的空教室查询接口中

---
+++ api/spare-classroom/index.js
@@ -0,0 +1,27 @@
+﻿const FormData = require('form-data');
+
+
+exports.route = {
+
+   /**
+   * GET /api/spare-classrom
+   * @apiParam cardnum  一卡通号
+   * @apiParam password 统一身份认证密码
+   **/
+  async get() {
+    let entries = Object.keys(this.query).map((key, index) => key + "=" + Object.values(this.query)[index]);
+    let formData = entries.reduce((pre, cur) => pre += "&" + cur);
+    let result = null;
+
+    if (this.query.campusId == "22") { // 教一~教七
+      result = (await this.axios.post(
+        "http://58.192.114.179/classroom/show/getemptyclassroomlist",
+        formData
+        )).data;
+    } else { // 纪忠楼 & 四牌楼
+
+    }
+
+    return result;
+  }
+}
