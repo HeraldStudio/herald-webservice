d396ae935412f900eaf326ef48e8d199ecf8976d
Vigilans <vigilans@foxmail.com>
2018-01-04 03:18:29 +0800

    空教室查询接口完成，并对模型设计进行了部分重构

--- api/spare-classroom/query.js
+++ api/spare-classroom/query.js
@@ -1,13 +1,57 @@
-﻿exports.isUndergrad() = function() {
+﻿const models = require("./models");
 
-}
+module.exports = {
+  hasSchoolInterface: ctx => ctx.query.campusId == 22, // 22为九龙湖（本科）区编号
 
-exports.undergrad = async () => await this.axios.post(
-  "http://58.192.114.179/classroom/show/getemptyclassroomlist",
-  this.querystring
-)
+  querySchoolInterface: async ctx => {
+    let result = (await ctx.axios.post(
+      "http://58.192.114.179/classroom/show/getemptyclassroomlist",
+      this.querystring
+    )).data
 
-exports.postgrad = async function () {
-  let records = require("./records.json");
-  
+    // 尝试剔除多余属性
+    result.rows.forEach((classroom, index) => {
+      try {
+        result.rows[index] = new models.Classroom(classroom);
+      } catch (ex) {
+        console.log(ex);
+      }
+    });
+
+    return result;
+  },
+
+  // 分页该怎么处理比较好？用缓存暂存一下找到的所有结果吗？
+  queryServerDatabase: async ctx => {
+    let query = ctx.query;
+    Object.keys(query).forEach(key => query[key] = (query[key] ? parseInt(query[key]) : null)); // 转换字符串属性至对应整数
+
+    // 利用课表筛选出该条件下有课的教室ID
+    let occupiedClassroomIds = models.classRecords.filter(record =>
+      (query.campusId == null ? true : record.campusId == query.campusId)
+      &&
+      (query.buildingId == null ? true : record.buildingId == query.buildingId)
+      &&
+      (query.startWeek <= record.endWeek && record.startWeek <= query.endWeek)
+      &&
+      (query.dayOfWeek == record.dayOfWeek)
+      &&
+      record.sequences.some(value => value >= query.startSequence && value <= query.endSequence)
+    ).map(record => record.classroomId);
+
+    // 筛选出所有无课的教室
+    let spareClassrooms = Object.values(models.classrooms).map(c => new models.Classroom(c)).filter(classroom => 
+      (query.campusId == null ? true : classroom.building.campusId == query.campusId)
+      &&
+      (query.buildingId == null ? true : classroom.buildingId == query.buildingId)
+      &&
+      !occupiedClassroomIds.includes(classroom.Id)
+    );
+
+    return {
+      "pager.pageNo": query.pageNo,
+      "pager.totalRows": spareClassrooms.length,
+      "rows": spareClassrooms.slice(query.pageSize * (query.pageNo - 1), query.pageSize * query.pageNo)
+    };
+  }
 }
