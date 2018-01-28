b85e44d0db5e96dd3f3fa5acf9b0dc818ba3299a
rikumi <riku.mi@outlook.com>
2018-01-28 10:36:18 +0800

    完善中间件文档

--- middleware/axios.js
+++ middleware/axios.js
@@ -1,29 +1,39 @@
 /**
- # 网络请求中间件
-   允许模块代码直接进行网络请求
+  # 网络请求中间件
 
- - `let res = (await this.get/post/put/delete('http://google.com')).data`（普通请求）
- - `let res = (await this.get/post/put/delete('/curriculum')).data`（递归请求 WS3）
+  允许模块代码直接进行网络请求。
+
+  ## 暴露接口
+
+  ctx.get:    (string, object?) => Promise<AxiosResponse>
+  ctx.post:   (string, object?, object?) => Promise<AxiosResponse>
+  ctx.put:    (string, object?, object?) => Promise<AxiosResponse>
+  ctx.delete: (string, object?) => Promise<AxiosResponse>
+
+  例：
+  - `let res = (await this.get/post/put/delete('http://google.com')).data`
  */
 const axios = require('axios')
 const { Semaphore } = require('await-semaphore')
 const config = require('../config.json')
 const sem = new Semaphore(10)
 
-// 关闭 HTTPS 网络请求的安全验证
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+/**
+  ## 安全性
 
-// 将 axios 的 get/post/put/delete 四个方法注入到 ctx 中
+  由于学校部分 HTTPS 的上游服务器可能存在证书问题，这里需要关闭 SSL 安全验证。
+ */
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+
 module.exports = async (ctx, next) => {
 
-  // 若请求相对路径，则递归请求 WS3，便于模块之间依赖
-  let _axios = axios.create({
-      baseURL: `http://localhost:${config.port}/`,
-      validateStatus: () => true,
-      ...config.axios
-    })
+/**
+  ## 实现
 
-  // 所有网络请求在线程池中执行，不超过 10 个线程
+  利用 10 线程的伪线程池 semaphore 进行网络请求，支持 get/post/put/delete 四个方法
+ */
+  let _axios = axios.create({ validateStatus: () => true, ...config.axios })
+
   ;['get','post','put','delete'].forEach(k => {
     ctx[k] = async function () {
       let release = await sem.acquire()
