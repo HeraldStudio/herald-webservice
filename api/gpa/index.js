b3b5160d59a255af519ebe72b07f1a034ec2f55c
rikumi <riku.mi@outlook.com>
2018-01-29 09:49:00 +0800

    WIP

---
+++ api/gpa/index.js
@@ -0,0 +1,20 @@
+const cv = require('node-opencv')
+
+exports.route = {
+  async get() {
+    let { cardnum, password } = this.user
+    let res = await this.get('http://xk.urp.seu.edu.cn/studentService/getCheckCode', {
+      responseType: 'arraybuffer'
+    })
+    if (res.status >= 400) {
+      this.throw(res.status)
+      return
+    }
+    let cookie = res.headers['set-cookie']
+    if (Array.isArray(cookie)) {
+      cookie = cookie[0]
+    }
+    // TODO 验证码识别
+    let verifyCode = await ...
+  }
+}
