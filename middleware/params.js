7ddf30d6881025b87598268abd515cd8a82be297
vhyme <vhyme@live.cn>
2018-02-04 17:06:51 +0800

    user API 增加姓名学号；自动 Cookie 机制；config.json 不再需要 example；四个基础模块小改

--- middleware/params.js
+++ middleware/params.js
@@ -4,9 +4,13 @@
   将 GET/DELETE 的 query 与 POST/PUT 的 body 合并成 ctx.params API。
   这里使用更严格的 HTTP 规范，GET/DELETE 不读 body，POST/PUT 不读 URL query。
 
+  ## 依赖接口
+
+  ctx.request.body    from koa-bodyparser
+
   ## 暴露接口
 
-  ctx.params  object
+  ctx.params          object
  */
 const bodyparser = require('koa-bodyparser')()
 
