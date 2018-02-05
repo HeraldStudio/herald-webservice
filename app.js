edbb26b46649fe45eb0f80d73fad3a0aff9c53ce
vhyme <vhyme@live.cn>
2018-02-05 17:24:39 +0800

    日志中间件；修正返回格式中间件一处错误处理问题

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const koa = require('koa')
 const app = new koa()
 const kf = require('kf-router')
-const logger = require('koa-logger')
 const config = require('./config.json')
 
 // 出错输出
 process.on('unhandledRejection', e => { throw e })
 process.on('uncaughtException', console.trace)
 
-// 日志中间件
-app.use(logger())
-
 // WS3 框架中间件，其中 redis 依赖 auth，不能倒置
+app.use(require('./middleware/logger'))
 app.use(require('./middleware/return'))
 app.use(require('./middleware/params'))
 app.use(require('./middleware/axios'))
