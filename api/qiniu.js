f7842a45544f571609f40cddef5e40a3abec5767
vhyme <vhyme@live.cn>
2018-02-07 21:17:29 +0800

    SDK规范化

--- api/qiniu.js
+++ api/qiniu.js
@@ -1,18 +1,8 @@
-const qiniu = require('qiniu')
-const config = require('../config.json')
-const crypto = require('crypto')
+const qiniu = require('../sdk/qiniu')
 
 exports.route = {
   async get() {
-    let { access, secret, bucket } = config.qiniu
-    let key = new Buffer(crypto.randomBytes(16)).toString('hex')
-    let mac = new qiniu.auth.digest.Mac(access, secret)
-    let uptoken = new qiniu.rs.PutPolicy({
-      scope: bucket + ':' + key,
-      saveKey: key,
-      returnBody: `{"url":"http://static.myseu.cn/${key}"}`
-    }).uploadToken(mac)
-
+    let uptoken = qiniu.getUptoken()
     return { uptoken }
   }
 }
