2b66f88f151ed3bf69386a3b4094600bbfa1b13c
rikumi <riku.mi@outlook.com>
2018-02-07 17:44:47 +0800

    修正object代理redis问题

--- middleware/redis.js
+++ middleware/redis.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'development') {
       pool[key] = value
     },
     async getAsync (key) {
-      return pool[key]
+      return pool[key] || 'null'
     }
   }
 } else {
