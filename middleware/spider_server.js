c14bb7eb91198fe59ef5e3ab94f45c6c148f2606
vhyme <vhyme@live.cn>
2018-02-07 10:11:32 +0800

    强迫症commit x2

--- middleware/spider_server.js
+++ middleware/spider_server.js
@@ -225,52 +225,46 @@ class SpiderServer {
   }
 
   // 用于参数处理
-  merge(/* obj1, obj2, obj3, ... */) {
+  merge() {
     function forEach(obj, fn) {
-      // Don't bother if no value provided
       if (obj === null || typeof obj === 'undefined') {
-        return;
+        return
       }
 
-      // Force an array if not already something iterable
       if (typeof obj !== 'object') {
-        /*eslint no-param-reassign:0*/
-        obj = [obj];
+        obj = [obj]
       }
 
       if (isArray(obj)) {
-        // Iterate over array values
         for (var i = 0, l = obj.length; i < l; i++) {
-          fn.call(null, obj[i], i, obj);
+          fn.call(null, obj[i], i, obj)
         }
       } else {
-        // Iterate over object keys
         for (var key in obj) {
           if (Object.prototype.hasOwnProperty.call(obj, key)) {
-            fn.call(null, obj[key], key, obj);
+            fn.call(null, obj[key], key, obj)
           }
         }
       }
     }
 
     function isArray(val) {
-      return toString.call(val) === '[object Array]';
+      return toString.call(val) === '[object Array]'
     }
 
-    let result = {};
+    let result = {}
     let assignValue = (val, key) => {
       if (typeof result[key] === 'object' && typeof val === 'object') {
-        result[key] = merge(result[key], val);
+        result[key] = merge(result[key], val)
       } else {
-        result[key] = val;
+        result[key] = val
       }
     }
 
-    for (let i = 0, l = arguments.length; i < l; i++) {
-      forEach(arguments[i], assignValue);
-    }
-    return result;
- }
+    [].slice.call(arguments).map(k => forEach(k, assignValue))
+
+    return result
+  }
 }
 
 const spiderServer = new SpiderServer()
