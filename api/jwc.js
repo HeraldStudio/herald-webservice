d39ca1c833f9a6bcbaf0a7d8854555d9b6b31ae3
MediosZ <823670050@qq.com>
2018-02-08 17:43:44 +0800

    教务处接口（修改

--- api/jwc.js
+++ api/jwc.js
@@ -1,56 +1,55 @@
 const cheerio = require('cheerio')
-const jwcUrl = 'http://jwc.seu.edu.cn'
+const baseUrl = 'http://jwc.seu.edu.cn'
+const list = [['#wp_news_w5', "教务信息"],['#wp_news_w6', "学籍管理"],['#wp_news_w7', "实践教学"],['#wp_news_w8', "国际交流"],['#wp_news_w9', "教学研究"],]
+
+/*
+返回格式
+[{
+  frag: string //所属栏目
+  title: string //题目
+  url: string
+  time： string //发布时间
+  isweb: bool //是否包含详情页
+  strong: bool //是否为重要
+},
+...]
+ */
 
 exports.route = {
-  /**
-   * GET /api/jwc
-   * 查询来自 jwc.seu.edu.cn 的通知
-   * 返回格式形如
-   * { "教务信息": [
-   *   { "title": "", "strong": false, "href": "", "date": 2018-01-25T00:00:00.000Z },
-   *   ...
-   *   ],
-   *   ...
-   * }
-   * "strong" 表示网站上是否以粗体显示。
-   **/
-  async get() {
-    function parseGroupName(item) {
-      return $('span span', item).text()
+  async get(){
+
+    let count = 0
+    let timeList = []
+    let data = {
+      item: [],
     }
 
-    function parseContent(item) {
-      return $('tbody > tr > td > table > tbody > tr', item).toArray().map(i => {
-        // 这个 row 里有三项
-        // 第一项含有标题，第二项是"NEW"标志，第三项是日期
-        let arr = $('td', i).toArray()
-        let titleElem = $('a', arr[0])
-        let dateElem = $('div', arr[2])
-        let cur = {
-          // 不用 text 是因为可能里面会有个 <font>
-          'title': titleElem.attr('title'),
-          // 如果里面套了一个 <font> 就说明是粗体显示的
-          'strong': titleElem.find('font').length ? true : false,
-          // 绝对路径
-          'href': jwcUrl + titleElem.attr('href'),
-          // 转化成Date
-          'date': new Date(dateElem.text())
-        }
-        return cur
-      })
-    }
-
-    let res = await this.get(jwcUrl)
+    let res = await this.get(baseUrl)
     let $ = cheerio.load(res.data)
-    let all = $('table.font3').toArray()
-    let ret = {}
-    // 取需要的信息
-    // [[<组名>, <内容>], ...]
-    // 下标是一个一个数出来的
-    ;[[3, 5], [6, 8], [9, 11], [12, 14], [15, 17]].map(
-      i => {
-        ret[parseGroupName(all[i[0]])] = parseContent(all[i[1]])
-      })
-    return ret
+
+    list.forEach(ele => {
+        $(ele[0]).find('div').toArray().filter(arr => {
+            return /\d+-\d+-\d+/.test($(arr).text())
+        }).map(item => {
+            timeList.push($(item).text())
+        })
+    })
+
+    list.forEach(ele => {
+        $(ele[0]).find('a').toArray().map(item => {
+            res = $(item)
+            data.item.push({
+                frag: ele[1],
+                tltle: res.attr('title'),
+                url: baseUrl + res.attr('href'),
+                time: timeList[count],
+                isWeb: /.+.htm$/.test(res.attr('href')),
+                strong: res.find('font').length ? true : false,
+            })
+            count++
+        })
+    })
+
+    return data.item
   }
 }
