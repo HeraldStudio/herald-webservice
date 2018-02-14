816f6d73508f454276f99d66a700eba537e80f89
Vigilans <vigilans@foxmail.com>
2018-02-14 16:33:10 +0800

    WIP

--- api/classroom/course.js
+++ api/classroom/course.js
@@ -1,12 +1,12 @@
-const db = require("../../database/helper")("classroom")
 const models = require("./models")
 
 exports.route = {
 
   /**
    * GET /api/classroom/course
+   * @remark （蹭课必备：根据课名搜教室）
    **/
   async get() {
-
+    // FLAG: TODO
   }
 }
