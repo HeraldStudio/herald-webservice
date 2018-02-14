ab894b74bf7788ea134d358685177a67ba59f74a
rikumi <riku.mi@outlook.com>
2018-02-14 11:24:21 +0800

    新ORM

--- api/canteen/canteen.js
+++ api/canteen/canteen.js
@@ -1,4 +1,4 @@
-const { db, isAdmin } = require('./db')
+const db = require('./db')
 
 exports.route = {
 
@@ -7,122 +7,30 @@ exports.route = {
    * 获取所有食堂信息
    **/
   async get() {
-    return await db.all('select * from canteen')
-  }
+    return await db.canteen.find()
+  },
 
   /**
    * POST /api/canteen/canteen
    * 管理员添加食堂
-   * @apiParam JSON {
-   *   canteen: {
-   *     name,
-   *     open_time_am, close_time_am,
-   *     open_time_pm, close_time_pm,
-   *     manager, manager_contact,
-   *     quanyi_contact,
-   *     comment
-   *   }
-   * }
+   * @apiParam JSON { canteen }
    **/
   async post() {
-    let { cardnum } = this.user
-    if (!isAdmin(cardnum)) { throw 403 }
-
+    if (!this.admin.canteen) { throw 403 }
     let { canteen } = this.params
-    let {
-      cid,
-      name,
-      open_time_am,
-      close_time_am,
-      open_time_pm,
-      close_time_pm,
-      manager,
-      manager_contact,
-      quanyi_contact,
-      comment
-    } = canteen
-
-    await db.run(`insert into canteen (
-      name,
-      open_time_am,
-      close_time_am,
-      open_time_pm,
-      close_time_pm,
-      manager,
-      manager_contact,
-      quanyi_contact,
-      comment
-    ) values (
-      ?, ?, ?, ?, ?, ?, ?, ?, ?
-    )`, [
-      name,
-      open_time_am,
-      close_time_am,
-      open_time_pm,
-      close_time_pm,
-      manager,
-      manager_contact,
-      quanyi_contact,
-      comment
-    ])
+    await db.canteen.insert(canteen)
   },
 
   /**
    * PUT /api/canteen/canteen
    * 管理员修改食堂信息
-   * @apiParam JSON {
-   *   canteen: {
-   *     cid, name,
-   *     open_time_am, close_time_am,
-   *     open_time_pm, close_time_pm,
-   *     manager, manager_contact,
-   *     quanyi_contact,
-   *     comment
-   *   }
-   * }
+   * @apiParam JSON { canteen }
    **/
   async put() {
-    let { cardnum } = this.user
-    if (!isAdmin(cardnum)) { throw 403 }
-
+    if (!this.admin.canteen) { throw 403 }
     let { canteen } = this.params
-    let {
-      cid,
-      name,
-      open_time_am,
-      close_time_am,
-      open_time_pm,
-      close_time_pm,
-      manager,
-      manager_contact,
-      quanyi_contact,
-      comment
-    } = canteen
-
-    await db.run(`update canteen set
-      name = ?,
-      open_time_am = ?,
-      close_time_am = ?,
-      open_time_pm = ?,
-      close_time_pm = ?,
-      manager = ?,
-      manager_contact = ?,
-      quanyi_contact = ?,
-      comment = ?
-    where
-      cid = ?
-    `, [
-      name,
-      open_time_am,
-      close_time_am,
-      open_time_pm,
-      close_time_pm,
-      manager,
-      manager_contact,
-      quanyi_contact,
-      comment,
-      cid
-    ])
+    let { cid } = canteen
+    await db.canteen.update({ cid }, canteen)
   },
 
   /**
@@ -130,11 +38,9 @@ exports.route = {
    * 管理员删除食堂
    * @apiParam cid
    **/
-  async put() {
-    let { cardnum } = this.user
-    if (!isAdmin(cardnum)) { throw 403 }
-
+  async delete() {
+    if (!this.admin.canteen) { throw 403 }
     let { cid } = this.params
-    await db.run('delete from canteen where cid = ?', [cid])
+    await db.canteen.remove({ cid })
   }
 }
