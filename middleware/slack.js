8646c84648455bed28d39b0e29222969aefe0749
rikumi <riku.mi@outlook.com>
2018-02-21 14:09:08 +0800

    xjb改

--- middleware/slack.js
+++ middleware/slack.js
@@ -65,23 +65,22 @@ class SlackMessage {
       attachment.callback_id = this.callback_id
       attachment.color = color
       attachment.actions = []
-      for (let i in actions) {
-        let button = actions[i]
-        if (!button.hasOwnProperty('name')) {
+      for (let action of actions) {
+        if (!action.hasOwnProperty('name')) {
           throw new Error('必须指定按钮的name')
         }
-        if (!button.hasOwnProperty('text')) {
+        if (!action.hasOwnProperty('text')) {
           throw new Error('必须指定按钮的text')
         }
-        if (!button.hasOwnProperty('response')) {
+        if (!action.hasOwnProperty('response')) {
           throw new Error('必须指定按钮的response')
         }
         attachment.actions.push({
-          name: button.name,
-          text: button.text,
+          name: action.name,
+          text: action.text,
           type: 'button',
-          style: button.style,
-          confirm: button.confirm
+          style: action.style,
+          confirm: action.confirm
         })
       }
 
