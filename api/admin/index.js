7ac29084613df08f46d5991833a24436c7404c6a
vhyme <vhyme@live.cn>
2018-02-22 17:05:10 +0800

    后台接口：当前请求数/爬虫数/redis状态/审核爬虫

--- api/admin/index.js
+++ api/admin/index.js
@@ -1,10 +1,12 @@
 exports.route = {
   get () {
+    this.related('control/pull', '[运维] Git 更新')
+    this.related('control/restart', '[运维] 系统重启')
+    this.related('status', '有关系统状态监测的接口')
     this.related('admin', {
       get: '{ domain?: string } 不带 domain [用户] 获取当前用户管理员身份；带 domain [同域/超管] 获取某域下管理员列表',
       post: '{ domain, admin: { name, cardnum, phone }} [同域/超管] 任命下级管理员',
       delete: '{ domain, cardnum } [上级同域/超管] 删除管理员'
     })
-    this.related('status', '有关系统状态监测的接口')
   }
 }
