56ef479fec34f9bc752a4af3089ef44f4ff33195
vhyme <vhyme@live.cn>
2018-02-26 14:17:53 +0800

    新的轮播/活动/公告系统

---
+++ api/admin/notice.js
@@ -0,0 +1,31 @@
+const db = require('../../database/publicity')
+
+exports.route = {
+  async get () {
+    if (!this.admin.maintenance) {
+      throw 403
+    }
+    return await db.notice.find()
+  },
+  async post () {
+    if (!this.admin.maintenance) {
+      throw 403
+    }
+    let { notice } = this.params
+    await db.notice.insert(notice)
+  },
+  async put () {
+    if (!this.admin.maintenance) {
+      throw 403
+    }
+    let { notice } = this.params
+    await db.notice.update({ nid: notice.nid }, notice)
+  },
+  async delete () {
+    if (!this.admin.maintenance) {
+      throw 403
+    }
+    let { nid } = this.params
+    await db.notice.remove({ nid })
+  }
+}
