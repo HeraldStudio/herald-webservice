f6caabc390b1f5f10227203b24cdbc7819c0a41b
Vigilans <vigilans@foxmail.com>
2018-02-27 00:03:59 +0800

    WIP

--- api/classroom/index.js
+++ api/classroom/index.js
@@ -4,7 +4,7 @@ exports.route = {
 
   /**
    * GET /api/classroom
-   * @apiParam type     要查询的数据类型，不区分大小写
+   * @apiParam type 要查询的数据类型，不区分大小写
    * @remark 获取某一数据表的所有条目的id与name
    * @note 前端可利用该API准备查询时的下拉框
    **/
@@ -30,10 +30,11 @@ exports.route = {
   async post() {
     // 此API会对数据库进行一次大更新。
     // 为方便处理，加载所有数据到内存中，并将相关方法改为内存版本实现
-    [Campus, Building, Classroom, ClassRecord].forEach((c, i, a) => {
-      ;[a[i]._all, a[i].all] = [a[i].all, await a[i].all()] // ClassName.all 获取所有对象
-      ;[a[i]._findId, a[i].findId] = [a[i].findId, (name) => a[i].all.find(o => o.name == name).Id]
-    })
+    
+    await Promise.all([Campus, Building, Classroom, ClassRecord].map(async (c, i, a) => {
+      a[i].all = await a[i].find({}) // ClassName.all 获取所有对象
+      a[i].findId = (name) => { const res = a[i].all.find(o => o.name === name); return res && res.Id }
+    }))
 
     // 方法复用，source提供原始网页数据，regex解析数据，parser将regex的一个match转换为对应的record
     // 整个函数完成一次对某一处课程信息的提取
@@ -42,29 +43,29 @@ exports.route = {
       for (let record; (record = parser(regex.exec(source))); ) {
         // 流式编程，处理需要新增校区/建筑/教室信息的情况
         [
-          [Capmus,    record.campusName]     // 校区，最上层
+          [Object,    record.notExistName],  // 哨兵顶层
+          [Campus,    record.campusName],    // 校区，上层
           [Building,  record.buildingName],  // 建筑，中层
-          [Classroom, record.classroomName], // 教室，下层
+          [Classroom, record.classroomName]  // 教室，下层
         ]
-        .map(v => new v[0]({ // 转换为对应的类对象
+        .map(v => v[1] && new v[0]({ // 转换为对应的类对象
           id: v[0].findId(v[1]),
           name: v[1]
         }))
-        .reduce((pre, cur) => { 
-          let [preName, curName] = [pre, cur].map(v => v.constructor.name.toLowerCase())
+        .reduce((pre, cur, i, a) => { 
+          let [preName, curName] = [pre, cur].map(v => v && v.constructor.name.toLowerCase())
           if (!cur.id) { // 如果数据库中没有对应id，则新生成一条记录保存
-            if (pre) { // 下层对象保存上一层Id       
-              cur[preName + "Id"] = pre.id    
-            }
-            // FIXME: 处理初始化时all为空的情况
-            cur.id = Math.max(...cur.constructor.all.map(o => o.id)) + 1 // 取最大Id + 1为新Id
-            cur.constructor.all.push(cur)
-            cur.save() // await-free
+            if (pre) { // 下层对象保存上一层Id
+              a[i][preName + "Id"] = pre.id 
+            }  
+            a[i].id = Math.max(...a[i].constructor.all.map(o => o.id), 0) + 1 // 取最大Id + 1为新Id，数组为空时最大Id为0
+            a[i].constructor.all.push(a[i])
+            a[i].save() // await-free
           }
           // 将record中各类name属性转换为Id属性
-          record[curName + "Id"] = cur.id
+          record[curName + "Id"] = a[i].id
           delete record[curName + "Name"]
-          return cur // 传递上层对象给下一层
+          return a[i] // 传递上层对象给下一层
         })
 
         record.save() // await-free
@@ -75,13 +76,13 @@ exports.route = {
     await Promise.all([
       // 更新研究生数据
       // FIXME: 补全院系编号
-      ...[000, 001].map(department => crawler(
+      ...[000, 001].map(async department => crawler(
         (await this.post(
-          "http://121.248.63.139/nstudent/pygl/kbcx_yx.aspx", 
-          "drpyx=000" // FIXME 还需要更多参数
+          "http://121.248.63.139/nstudent/pygl/kbcx_yx.aspx"//, 
+          //"drpyx=000" // FIXME 还需要更多参数
         )).data,
-        /<td>(.+)<\/td><td>(.+)<\/td><td>([^\s]+)\s*<\/td><td>第(\d+)-(\d+)周 (.+)-(.+)<\/td><td>(.+)<\/td><td>([^\w]+)([0-9a-zA-Z]+)<\/td><td>(\d+)<\/td><td>(\d+)<\/td>/img,
-        entry => new ClassRecord({
+        /<td>(.+)<\/td><td>(.+)<\/td><td>([^\s]+)\s*<\/td><td>第(\d+)-(\d+)周;? (.+)-(.+)<\/td><td>(.+)<\/td><td>([^\w]+)([0-9a-zA-Z]+)<\/td><td>(\d+)<\/td><td>(\d+)<\/td>/img,
+        entry => entry && new ClassRecord({
           name          : entry[1],
           courseId      : entry[2],
           courseName    : entry[3],
--- api/classroom/models.js
+++ api/classroom/models.js
@@ -1,94 +1,4 @@
-const db = require("../../database/helper")("classroom")
-
-// 面向Sqlite的ORM基类
-class ModelBase { 
-  constructor(schema, init) {
-    Object.assign(this, {
-      // id与name为所有子类都会有的属性（在classroom API的context下）
-      // 如有必要，通过学校接口返回的JSON中的杂七杂八的属性也可设置于此
-      id   : 0,
-      name : ""
-    }, schema, init) // schema为默认构造对象，init为外部传入的用以初始化对象
-  }
-
-  // 异步创建一个对象，按需被子类重写（如子类对象创建时需读取数据库）
-  static async create() {
-    // 若未被子类重写，则通过构造函数正常创建一个对象
-    return new this()
-  }
-
-  // 初始化数据库表
-  static async initDb() {
-    const entries = Object.entries(await this.create()) // 通过this.create()构造一个默认对象作为schema
-      .filter(e => e[0][0] !== '_') // 过滤掉采用Lazy Load机制的（复杂）属性
-      .slice(1) // 过滤掉作为主键的id属性，同时name属性保证keys的长度至少为1
-
-    // 对于静态方法，this指向的就是constructor
-    // 即this === this.prototype.constructor -> true
-    // 因此this.name即为当前实例化的对象的类名
-    await db.run(`
-      create table if not exists ${this.name} (
-        id    integer    primary key,
-        ${entries.map(e => `${e[0]} ${typeof e[1] === "number"? "integer" : "text"} not null`).toString()}
-      )
-    `)
-  }
-
-  // 通过名字找到对应记录的ID
-  static async findId(name) {
-    const row = await db.get(`select id from ${this.name} where name = ?`, [name])
-    return row && row.id
-  }
-
-  // 根据id从数据库加载对象
-  static async load(id) {
-    const row = await db.get(`select * from ${this.name} where id = ?`, [id])
-    return new this(row) // this指代见上
-  }
-
-  // 从数据库中加载所有对象
-  static async all() {
-    const rows = await db.all(`select * from ${this.name}`)
-    return rows.map(row => new this(row))
-  }
-
-  // 保存当前对象到数据库
-  async save() {
-    const values = Object.entries(this)
-      .filter(v => v[0][0] !== '_') // 过滤掉采用Lazy Load机制的（复杂）属性
-      .map(v => v[1] instanceof Array ? v[1].toString() : v[1]) // 数组采用字符串保存
-
-    await db.run(`
-      insert or replace into ${this.constructor.name} values (
-        ${values.map(v => '?').toString()}
-      )`, values
-    )
-  }
-
-  // 将类对象转化为正规形式，方便作为json返回
-  async formalize() {
-    let lazyKeys = Object.keys(this).filter(k => k[0] === '_')
-    for (k of lazyKeys) {
-      this[k.slice(1)] = await this[k.slice(1)] // 加载lazy属性
-      delete this[k] // 删除'_'开头的辅助属性
-      await this[k.slice(1)].formalize() // 上一层级属性递归调用
-    }
-    return this
-  }
-
-  // 定义一个采用Lazy load机制读取的属性
-  // 用于将当前对象的外键ID绑定到对应的对象上
-  defineLazyProperty(propName, loadFunc, initVal = undefined) {
-    // 若不需Lazy load，可为该属性先行绑定一个初值
-    this['_' + propName] = initVal
-    Object.defineProperty(this, propName, {
-      // 由于涉及到数据库读取，若触发lazy load, getter会返回Promise
-      // 因此，使用getter时需加await保证返回正确的属性
-      get: () => this['_' + propName] || loadFunc().then(v => this['_' + propName] = v),
-      set: v => this['_' + propName] = v 
-    })
-  }
-}
+const { db, ModelBase } = require("./orm")("classroom")
 
 /**
   ## Campus 数据表结构
@@ -97,7 +7,7 @@ class ModelBase {
   name    text       校区名 
  */
 class Campus extends ModelBase {
-  constructor(init) {
+  constructor(init = {}) {
     super({}, init)
   }
 
@@ -129,7 +39,7 @@ class Campus extends ModelBase {
   campusId    integer    建筑所属校区Id
  */
 class Building extends ModelBase {
-  constructor(init) {
+  constructor(init = {}) {
     super({
       campusId: 0
     }, init)
@@ -146,7 +56,7 @@ class Building extends ModelBase {
   buildingId    integer    教室所属建筑Id
  */
 class Classroom extends ModelBase {
-  constructor(init) {
+  constructor(init = {}) {
     super({
       buildingId : 0,
       classroomTypeIdList: []
@@ -157,16 +67,11 @@ class Classroom extends ModelBase {
       () => Promise.all(this.classroomTypeIdList.map(Id => ClassroomType.load(Id))),
       init.classroomTypeList && init.classroomTypeList.map(v => new ClassroomType(v)) // node要是支持optional chaining就好了...
     )
-    // TODO: 在基类中加入数组类型解析
-    // 将从数据库中读出的用于保存的字符串转为数组
-    if (typeof this.classroomTypeIdList == "string") {
-      this.classroomTypeIdList = this.classroomTypeIdList.split(',').map(Number)
-    }
   }
 }
 
 /**
-  ## Classroom 数据表结构
+  ## ClassroomType 数据表结构
 
   id    integer    教室类型Id
   name  text       教室类型名，可能值有"空调"，"多媒体"，"录播"，"设计"，"电教"，"语音"，"活动"
