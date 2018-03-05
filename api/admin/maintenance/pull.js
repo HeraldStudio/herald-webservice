1c2bd82edf49b62c01578fca6d88056c9bc5cb3e
rikumi <riku.mi@outlook.com>
2018-03-05 20:15:17 +0800

    pull接口更新

--- api/admin/maintenance/pull.js
+++ api/admin/maintenance/pull.js
@@ -14,18 +14,7 @@ exports.route = {
     if (!this.admin.maintenance) {
       throw 403
     }
-    let res1 = await exec('git pull')
-    let { stdout, stderr } = res1
-    let changed = stdout !== 'Already up-to-date.'
-    if (changed) {
-      let res2 = await exec('yarn')
-      stdout = [stdout, res2.stdout].join('\n').trim()
-      stderr = [stderr, res2.stderr].join('\n').trim()
-    }
-    let out = (stdout + '\n' + stderr).trim()
-    if (changed && this.admin.super) {
-      throw 401
-    }
-    return { changed, out }
+    let res1 = await exec('git pull; yarn')
+    return
   }
 }
