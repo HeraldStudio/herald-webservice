94dd7351b005bdc3dc7bb40af956e5e8d3a9939c
rikumi <riku.mi@outlook.com>
2018-03-17 00:24:30 +0800

    修正adapter重定向问题

--- middleware/adapter/unpacker.js
+++ middleware/adapter/unpacker.js
@@ -4,6 +4,8 @@ module.exports = async (ctx, next) => {
     if (ctx.body.code >= 400) {
       ctx.body = ''
       ctx.throw(ctx.body.code)
+    } else if (/Redirecting to <a href="(.*)">\1<\/a>\./.test(ctx.body.result)) {
+      ctx.redirect(RegExp.$1)
     } else {
       ctx.body = ctx.body.result || 'OK'
     }
