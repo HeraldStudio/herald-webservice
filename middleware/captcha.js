4a9c8dc421b42efd84f4c1adb51f601c28d16012
vhyme <vhyme@live.cn>
2018-03-18 20:46:20 +0800

    验证码测试

--- middleware/captcha.js
+++ middleware/captcha.js
@@ -11,19 +11,23 @@ const iconv = require('iconv');
 const fs = require('fs');
 const captchaJobPool = {};
 
-let library_active = false;
-let jwc_active = false;
+let libraryActive = false;
+let jwcActive = false;
 
-const library_process = spawn('/usr/local/bin/anaconda3/envs/captcha/bin/python', [process.cwd() + '/middleware/captcha_childprocess/libcaptcha.py']);
-const jwc_process = spawn('/usr/local/bin/anaconda3/envs/captcha/bin/python', [process.cwd() + '/middleware/captcha_childprocess/jwccaptcha.py']);
+const libraryProcess = spawn('/usr/local/bin/anaconda3/envs/captcha/bin/python', [
+  process.cwd() + '/middleware/captcha-childprocess/libcaptcha.py'
+]);
+const jwcProcess = spawn('/usr/local/bin/anaconda3/envs/captcha/bin/python', [
+  process.cwd() + '/middleware/captcha-childprocess/jwccaptcha.py'
+]);
 
-jwc_process.stdout.on('data', (chunk) => {
+jwcProcess.stdout.on('data', (chunk) => {
   "use strict";
   let message = chunk.toString();
   message = message.substr(0, message.length-1);
   if (message === 'loaded') {
      console.log('教务处验证码模型加载成功');
-    jwc_active = true;
+    jwcActive = true;
   } else {
     try {
       message = JSON.parse(message);
@@ -34,13 +38,13 @@ jwc_process.stdout.on('data', (chunk) => {
   }
 });
 
-library_process.stdout.on('data', (chunk) => {
+libraryProcess.stdout.on('data', (chunk) => {
   "use strict";
   let message = chunk.toString();
   message = message.substr(0, message.length-1);
   if (message === 'loaded') {
     console.log('图书馆验证码模型加载成功');
-    library_active = true;
+    libraryActive = true;
   } else {
     try {
       message = JSON.parse(message);
@@ -52,12 +56,12 @@ library_process.stdout.on('data', (chunk) => {
 });
 
 
-jwc_process.stdin.on('error', (err) => {
+jwcProcess.stdin.on('error', (err) => {
   "use strict";
   console.log(err);
 });
 
-library_process.stdin.on('error', (err) => {
+libraryProcess.stdin.on('error', (err) => {
   "use strict";
   console.log(err);
 });
@@ -66,28 +70,32 @@ const generateName = () => {
   return Math.random().toString(36).substr(2)
 };
 
-const jwcCaptcha = async (ctx) => {
+const jwcCaptcha = async ctx => {
   let pic = Buffer.from((await ctx.get('http://xk.urp.seu.edu.cn/studentService/getCheckCode')).data);
   return new Promise((resolve, reject) => {
-      let picName = `./captcha_temp/${generateName()}.jpg`;
-      fs.writeFile(picName, pic, (err) => {
-        "use strict";
-        if (err || !jwc_active) { reject(Error('fail_to_get_captcha'))} else {
-          jwc_process.stdin.write(picName + '\n');
-          captchaJobPool[picName] = {resolve, reject}
-        }
-      });
+    let picName = `./captcha-temp-${generateName()}.jpg`;
+    fs.writeFile(picName, pic, err => {
+      "use strict";
+      if (err || !jwcActive) {
+        reject(err || Error('Failed to get captcha'))
+      } else {
+        jwcProcess.stdin.write(picName + '\n');
+        captchaJobPool[picName] = {resolve, reject}
+      }
+    });
   });
 };
 
-const libraryCaptcha = async (ctx) => {
+const libraryCaptcha = async ctx => {
   let pic = Buffer.from((await ctx.get('http://www.libopac.seu.edu.cn:8080/reader/captcha.php')).data);
   return new Promise((resolve, reject) => {
-    let picName = `./captcha_temp/${generateName()}.png`;
-    fs.writeFile(picName, pic, (err) => {
+    let picName = `./captcha-temp-${generateName()}.png`;
+    fs.writeFile(picName, pic, err => {
       "use strict";
-      if (err || !jwc_active) { reject(Error('fail_to_get_captcha'))} else {
-        library_process.stdin.write(picName + '\n');
+      if (err || !jwcActive) {
+        reject(err || Error('Failed to get captcha'))
+      } else {
+        libraryProcess.stdin.write(picName + '\n');
         captchaJobPool[picName] = {resolve, reject}
       }
     });
@@ -95,7 +103,13 @@ const libraryCaptcha = async (ctx) => {
 };
 
 module.exports = async (ctx, next) => {
-  ctx.jwcCaptcha = () => { return Promise.race([jwcCaptcha(ctx), new Promise((resolve, reject) => {setTimeout(reject, 3000)})]) };
-  ctx.libraryCaptcha = () => {return Promise.race([libraryCaptcha(ctx), new Promise((resolve, reject) => {setTimeout(reject, 3000)})]) };
+  ctx.jwcCaptcha = () => Promise.race([
+    jwcCaptcha(ctx),
+    new Promise((_, reject) => setTimeout(reject, 3000))
+  ])
+  ctx.libraryCaptcha = () => Promise.race([
+    libraryCaptcha(ctx),
+    new Promise((_, reject) => setTimeout(reject, 3000))
+  ])
   await next()
 };
