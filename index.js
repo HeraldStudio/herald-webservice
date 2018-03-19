1a95ef840b0ded8e43ab68fbd22845ada7af4507
vhyme <vhyme@live.cn>
2018-03-19 23:44:39 +0800

    恢复首页接口互解释

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 exports.route = {
   get() {
-    this.redirect('https://github.com/heraldstudio/herald-webservice')
+    this.related('api', '所有功能接口')
+    this.related('auth', {
+      post: '{ cardnum, password, platform: /^[a-z\\-]$/ } 登录，得到的 token 带在 header.token 中使用'
+    })
+    return 'Herald WebService3'
   }
 }
