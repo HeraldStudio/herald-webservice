2038c7dede40a6f4567a0e4e9aeb8d1042cf9492
ThisTusooa <tusooa@vista.aero>
2018-03-20 08:28:55 +0800

    notice

--- api/notice.js
+++ api/notice.js
@@ -2,26 +2,8 @@ const cheerio = require('cheerio')
 const Europa = require('node-europa')
 const db = require('../database/publicity')
 
-const sites = {
-  jwc: {
-    baseUrl: 'http://jwc.seu.edu.cn',
-    infoUrl: 'http://jwc.seu.edu.cn',
-    list: [
-      ['#wp_news_w5', "教务信息"],
-      ['#wp_news_w6', "学籍管理"],
-      ['#wp_news_w7', "实践教学"],
-      ['#wp_news_w8', "国际交流"],
-      ['#wp_news_w9', "教学研究"]
-    ],
-    contentSelector: '[portletmode="simpleArticleContent"]'
-  },
-  zwc: {
-    baseUrl: 'http://zwc.seu.edu.cn',
-    infoUrl: 'http://zwc.seu.edu.cn/4297/list.htm',
-    list: [['#wp_news_w3', '总务处公告']],
-    contentSelector: '[portletmode="simpleArticleContent"]' // 两个平台的正文选择器是一样的
-  },
-}
+const { sites, fetchNews } = require('./notice/func.js')
+const commonSites = ['jwc', 'zwc']
 // 5 天之内的信息，全部留下
 const keepTime = 1000 * 60 * 60 * 24 * 5
 // 10 条以内的信息，全部留下
@@ -31,49 +13,16 @@ exports.route = {
 
   /**
    * GET /api/notice
-   * @apiReturn [{ category, title, url, time, isAttachment, isImportant }]
+   * @apiReturn [{ category, department, title, url, time, isAttachment, isImportant }]
    */
   async get () {
     let now = new Date().getTime()
-    let ret = []
-    for (const site of Object.keys(sites)) {
-      let res = await this.get(sites[site].infoUrl)
-      let $ = cheerio.load(res.data)
-      let list = sites[site].list
-
-      let timeList = list.map
-      (ele =>
-       $(ele[0]).find('div').toArray()
-       .filter(arr => /\d+-\d+-\d+/.test($(arr).text()))
-       .map(item => new Date($(item).text()).getTime())
-      ).reduce((a, b) => a.concat(b), [])
-
-      ret = ret.concat(
-        list.map(
-          ele =>
-            $(ele[0]).find('a').toArray()
-            .map(k => $(k)).map(k => {
-              let href = k.attr('href')
-              return {
-                category: ele[1],
-                title: k.attr('title'),
-                url: /^\//.test(href)
-                  ? sites[site].baseUrl + href
-                  : href,
-                isAttachment: !/.+.html?$/.test(k.attr('href')),
-                isImportant: !!k.find('font').length,
-              }
-            })
-        ).reduce((a, b) => a.concat(b), []).map((k, i) => {
-          k.time = timeList[i]
-          return k
-        }))
-    }
-
+    let ret = fetchNews(commonSites)
     // 小猴系统通知
     ret = ret.concat((await db.notice.find()).map(k => {
       return {
         category: '系统通知',
+        department: '小猴偷米',
         title: k.title,
         nid: k.nid,
         time: k.publishTime
