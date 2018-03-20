fdb5518db27c2e0eafff9b4fbaebd74b002e6297
ThisTusooa <tusooa@vista.aero>
2018-03-20 11:57:33 +0800

    getTime()

--- api/notice.js
+++ api/notice.js
@@ -89,7 +89,7 @@ exports.route = {
                 // url 中的日期未必准确
                 let match = /\/(\d{4})\/(\d{2})(\d{2})\//.exec(k.url)
                 if (match !== null) {
-                  k.time = new Date(match[1] + '-' + match[2] + '-' + match[3])
+                  k.time = new Date(match[1] + '-' + match[2] + '-' + match[3]).getTime()
                 }
               }
               return k
