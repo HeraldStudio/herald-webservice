71bd7234cfb5f21fd14a9d0ec0ac73bda297e358
vhyme <vhyme@live.cn>
2018-03-27 20:09:33 +0800

    大幅优化后台管理监控的数据库查询语句

--- routes/api/admin/maintenance/daily.js
+++ routes/api/admin/maintenance/daily.js
@@ -19,31 +19,41 @@ Array.prototype.groupBy = function(keyOfKey, keyOfValue) {
   })
 }
 
+// 24 小时接口调用统计
 exports.route = {
   async get() {
     if (!this.admin.maintenance) {
       throw 403
     }
 
-    // 24 小时接口调用统计
     let yesterday = new Date().getTime() - 1000 * 60 * 60 * 24
-    let dailyStat = await db.stat.find({ time: { $gte: yesterday } })
-    let totalCount = dailyStat.length
 
-    // 过滤管理接口
-    dailyStat = dailyStat.filter(k => !/^\/api\/admin\//.test(k.route)).map(k => {
+    // 根据需求构造更快速的数据库查询
+    // 按时间片、路由、方法、状态、请求数量分组，这几项都相同的进行累计
+    let dailyStat = await db.raw(`
+      select
+        (time - ${yesterday}) / 1800000 as period,
+        route, method, status,
+        round(avg(duration)) as averageDuration,
+        count(*) as count
+      from stat
+      where time > ${yesterday}
+      group by period, route, method, status;
+    `)
+    let totalCount = dailyStat.length
+    dailyStat = dailyStat.map(k => {
       k.operation = k.method + ' ' + k.route
-      k.period = Math.floor((k.time - yesterday) / (1000 * 60 * 30))
       return k
     })
 
-    // 得到 [0, 48) 的整数区间，对其使用 map
+    // 不直接拿数据库结果做 map，防止遗漏没有请求的时间片
+    // 首先得到 [0, 48) 的整数区间，对其使用 map
     return [].slice.call([].fill.call({ length: 24 * 2 }, 0)).map((_, i) => {
 
       // 对于 [0, 48) 的每一个整数，在日志中查找该时间范围内的请求
       let operations = dailyStat.filter(k => k.period === i)
 
-      // 返回能够概括该时间范围内所有请求情况的一个对象
+      // 返回能够概括该时间范围内所有类型请求情况的一个对象
       return {
 
         // 时间范围的开始和结束戳
@@ -62,27 +72,11 @@ exports.route = {
             operation: group.operation,
 
             // 操作结果再按 status 状态码分组
-            results: group.results.groupBy('status', 'requests').map(group => {
-
-              // 经过 groupBy 分组操作后，每一组都包含 status 整数和对应的 requests 数组
-              // 这里每一个 group 代表同一个操作的每一种不同结果
-              // 对于每个结果，这里需要转化为概括该请求结果的出现次数、平均用时等详细情况的一个对象
-              return {
-
-                // 状态码保留不变
-                status: group.status,
-
-                // 得到该结果的请求的平均耗时
-                averageDuration: group.requests.length ? Math.round(
-                  group.requests
-                    .map(request => request.duration)
-                    .reduce((a, b) => a + b, 0) / group.requests.length
-                  ) : 0,
-
-                // 得到该结果的请求出现的次数
-                count: group.requests.length
-              }
-            }),
+            results: group.results.map(k => ({
+              status: k.status,
+              count: k.count,
+              averageDuration: k.averageDuration
+            })),
 
             // 执行该操作的请求的总次数
             count: group.results.length
