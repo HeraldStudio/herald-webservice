ae3c7b303f5e631ff0a9ef23548a1a3b502219e5
rikumi <riku.mi@outlook.com>
2018-03-31 22:22:59 +0800

    修正跨域

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,15 +1,24 @@
-// 允许的第三方前端地址正则表达式
-// 注意开头要加 ^ 结尾要加 $ 以防止利用部分域名攻击
-const allowOrigins = [
-  /^(localhost|127\.0\.0\.1)(:\d+)?$/
+// 允许的第三方前端域名，精确匹配
+const allowDomains = [
+  'myseu.cn',
+  'app.heraldstudio.com',
+  'www.heraldstudio.com',
+  'localhost',
+  '127.0.0.1'
 ]
 
 module.exports = async (ctx, next) => {
   let { origin } = ctx.request.headers
-  if (origin && allowOrigins.find(r => r.test(origin))) {
-    ctx.set('Access-Control-Allow-Origin', origin)
-    ctx.set('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,DELETE,PATCH')
-    ctx.set('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,token')
+  if (origin) {
+    // 不考虑端口和协议
+    origin = /[^/]*$/.exec(origin)[0]
+    origin = /^[^:]*/.exec(origin)[0]
+
+    if (origin && ~allowDomains.indexOf(origin)) {
+      ctx.set('Access-Control-Allow-Origin', origin)
+      ctx.set('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,DELETE,PATCH')
+      ctx.set('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,token')
+    }
   }
   await next()
 }
