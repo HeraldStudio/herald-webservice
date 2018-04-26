66b3d449b9f98e81efd18960511bf91cd29254bf
rikumi <riku.mi@outlook.com>
2018-04-26 18:54:49 +0800

    自制进程管理轮子，接管验证码worker

--- middleware/captcha.js
+++ middleware/captcha.js
@@ -1,96 +1,41 @@
-const { spawn } = require('child_process');
-const axios = require('axios');
-const chardet = require('chardet');
-const iconv = require('iconv');
-const fs = require('fs');
+const procmon = require('../worker/procmon')
+const fs = require('fs')
 
 module.exports = ({ python }) => {
-  const captchaJobPool = {};
+  const captchaJobPool = {}
+  let libraryActive = false
 
-  let libraryActive = false;
-  let jwcActive = false;
-
-  const libraryProcess = spawn(python, [
+  const libraryProcess = new procmon(python, [
     process.cwd() + '/worker/captcha/libcaptcha.py'
-  ]);
-  // const jwcProcess = spawn(python, [
-  //   process.cwd() + '/worker/captcha/jwccaptcha.py'
-  // ]);
-  
-  process.on('exit', () => {
-    libraryProcess.kill()
-    // jwcProcess.kill()
+  ], {
+    maxCpu: 50,
+    autoReload: true,
+    onBind: proc => {
+      proc.stdout.on('data', (chunk) => {
+        "use strict";
+        let message = chunk.toString();
+        message.trim().split('\n').map(message => {
+          if (message === 'loaded') {
+            libraryActive = true
+          } else {
+            try {
+              message = JSON.parse(message)
+              let job = captchaJobPool[message.path]
+              delete captchaJobPool[message.path]
+              let result = message.result
+              job.resolve(result)
+            } catch (e) {}
+          }
+        })
+      })
+    }
   })
 
-  // jwcProcess.stdout.on('data', (chunk) => {
-  //   "use strict";
-  //   let message = chunk.toString();
-  //   message.trim().split('\n').map(message => {
-  //     if (message === 'loaded') {
-  //       // console.log('[python] 教务处验证码模型加载成功');
-  //       jwcActive = true;
-  //     } else {
-  //       try {
-  //         message = JSON.parse(message);
-  //         let job = captchaJobPool[message.path];
-  //         delete captchaJobPool[message.path];
-  //         let result = message.result;
-  //         job.resolve(result)
-  //       } catch (e) {}
-  //     }
-  //   })
-  // });
-
-  libraryProcess.stdout.on('data', (chunk) => {
-    "use strict";
-    let message = chunk.toString();
-    message.trim().split('\n').map(message => {
-      if (message === 'loaded') {
-        // console.log('[python] 图书馆验证码模型加载成功');
-        libraryActive = true;
-      } else {
-        try {
-          message = JSON.parse(message);
-          let job = captchaJobPool[message.path];
-          delete captchaJobPool[message.path];
-          let result = message.result;
-          job.resolve(result)
-        } catch (e) {}
-      }
-    })
-  });
-
-  const handleError = err => {
-    // err.toString().split('\n')
-    //   .map(k => k.trim())
-    //   .filter(k => k)
-    //   .filter(k => !/WARNING/.test(k))
-    //   .map(k => console.log('[python]', k))
-  }
-
-  // jwcProcess.stderr.on('data', handleError);
-  libraryProcess.stderr.on('data', handleError);
+  libraryProcess.start()
 
   const generateName = () => {
     return Math.random().toString(36).substr(2)
-  };
-
-  // const jwcCaptcha = async ctx => {
-
-  //   let pic = Buffer.from((await ctx.get('http://xk.urp.seu.edu.cn/studentService/getCheckCode')).data);
-  //   return new Promise((resolve, reject) => {
-  //     let picName = `/tmp/ws3-captcha-${generateName()}.jpg`;
-  //     fs.writeFile(picName, pic, err => {
-  //       "use strict";
-  //       if (err || !jwcActive) {
-  //         reject(err || Error('Failed to get captcha'))
-  //       } else {
-  //         jwcProcess.stdin.write(picName + '\n');
-  //         captchaJobPool[picName] = {resolve, reject}
-  //       }
-  //     });
-  //   });
-  // };
+  }
 
   const libraryCaptcha = async ctx => {
     let pic = Buffer.from((await ctx.get('http://www.libopac.seu.edu.cn:8080/reader/captcha.php')).data);
@@ -101,22 +46,18 @@ module.exports = ({ python }) => {
         if (err || !libraryActive) {
           reject(err || Error('Failed to get captcha'))
         } else {
-          libraryProcess.stdin.write(picName + '\n');
+          libraryProcess.process.stdin.write(picName + '\n');
           captchaJobPool[picName] = {resolve, reject}
         }
-      });
-    });
-  };
+      })
+    })
+  }
 
   return async (ctx, next) => {
-    // ctx.jwcCaptcha = () => Promise.race([
-    //   jwcCaptcha(ctx),
-    //   new Promise((_, reject) => setTimeout(() => reject('验证码解析失败'), 3000))
-    // ])
     ctx.libraryCaptcha = () => Promise.race([
       libraryCaptcha(ctx),
       new Promise((_, reject) => setTimeout(() => reject('验证码解析失败'), 3000))
     ])
     await next()
-  };
+  }
 }
