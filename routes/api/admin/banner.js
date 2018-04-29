c373cdbf83c400125f50c653c4908a676c6c3a95
vhyme <vhyme@live.cn>
2018-04-29 15:58:14 +0800

    点击量统计

--- routes/api/admin/banner.js
+++ routes/api/admin/banner.js
@@ -1,11 +1,14 @@
 const db = require('../../../database/publicity')
 
 exports.route = {
-  async get () {
+  async get ({ page = 1, pagesize = 10 }) {
     if (!this.admin || !this.admin.publicity) {
       throw 403
     }
-    return (await db.banner.find()).sort((a, b) => b.startTime - a.startTime)
+    return await Promise.all(await db.banner.find({}, pagesize, (page - 1) * pagesize, 'startTime-'))
+      .map(async k => {
+        k.clicks = await db.bannerClick.count({ bid: k.bid })
+      })
   },
   async post ({ banner }) {
     if (!this.admin || !this.admin.publicity) {
@@ -26,6 +29,7 @@ exports.route = {
       throw 403
     }
     await db.banner.remove({ bid })
+    await db.bannerClick.remove({ bid })
     return 'OK'
   }
 }
