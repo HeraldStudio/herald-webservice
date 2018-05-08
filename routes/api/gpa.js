5ba9269ecad73da302665fe31df367a07a9a7090
rikumi <riku.mi@outlook.com>
2018-05-08 12:10:13 +0800

    课表统计！

--- routes/api/gpa.js
+++ routes/api/gpa.js
@@ -1,4 +1,5 @@
 const cheerio = require('cheerio')
+const db = require('../../database/course')
 
 exports.route = {
 
@@ -10,7 +11,7 @@ exports.route = {
   async get() {
     return await this.userCache('1h+', async () => {
       await this.useAuthCookie()
-      let { cardnum } = this.user
+      let { cardnum, schoolnum } = this.user
 
       // 本科生
       if (/^21/.test(cardnum)) {
@@ -18,17 +19,70 @@ exports.route = {
           'http://xk.urp.seu.edu.cn/studentService/cs/stuServe/studentExamResultQuery.action'
         )
         let $ = cheerio.load(res.data)
-        let detail = $('#table2 tr').toArray().slice(1).map(tr => {
-          let [semester, courseId, courseName, credit, score, scoreType, courseType]
+        let detail = (await Promise.all($('#table2 tr').toArray().slice(1).map(async tr => {
+          let [semester, cid, courseName, credit, score, scoreType, courseType]
             = $(tr).find('td').toArray().slice(1).map(td => {
               return $(td).text().trim().replace(/&[0-9A-Za-z];/g, '')
             })
 
           // 学分解析为浮点数；成绩可能为中文，不作解析
           credit = parseFloat(credit)
-          return { semester, courseId, courseName, courseType, credit, score, scoreType }
 
-        }).reduce((a, b) => { // 按学期分组
+          try /* 课程统计代码 */ {
+            // 课程插入课程统计数据库
+            let numberScore = parseFloat(score) || 0
+            
+            if (!numberScore) {
+              if (/优/.test(score)) {
+                numberScore = 95
+              } else if (/良/.test(score)) {
+                numberScore = 85
+              } else if (/中/.test(score)) {
+                numberScore = 75
+              } else if (/不及格/.test(score)) {
+                numberScore = 0
+              } else if (/及格/.test(score)) {
+                numberScore = 60
+              }
+            }
+            let course = await db.course.find({ cid }, 1)
+            if (!course) {
+              await db.course.insert(course = { cid, courseName, courseType, credit, avgScore: 0, sampleCount: 0 })
+            }
+
+            // 若分数可识别为数字且非零，更新课程的平均分
+            // course.sampleCount 是原来的样本容量，sampleCount 是更新后的样本容量
+            let sampleCount = course.sampleCount + 1
+            let avgScore = numberScore ? (course.avgScore * course.sampleCount + numberScore) / sampleCount : course.avgScore
+            await db.course.update({ cid }, { courseName, courseType, credit, avgScore, sampleCount })
+
+            // 课程学期信息插入课程学期表关系表
+            if (scoreType !== '重修') {
+              let major = schoolnum.substr(0, 3)
+
+              // 入学年份后两位，必须在一卡通号里取，学号可能因为留级而变化
+              let entryYear = parseInt(cardnum.substr(3, 2))
+
+              // 当前课程所在学期的年份后两位，取学期号前两位
+              let semesterYear = parseInt(semester.substr(0, 2))
+
+              // 计算得到当前课程的年级，需要加一，使得大一为 1
+              let grade = semesterYear - entryYear + 1
+
+              // 学期次序，1 短学期，2 秋学期，3春学期
+              let semesterIndex = parseInt(semester.split('-')[2])
+
+              // 查找是否已有该记录，无该记录则插入
+              let record = { cid, major, grade, semester: semesterIndex }
+              let courseSemester = await db.courseSemester.find(record, 1)
+              if (!courseSemester) {
+                await db.courseSemester.insert(record, 1)
+              }
+            }
+          } catch (e) {}
+          return { cid, semester, courseName, courseType, credit, score, scoreType }
+
+        }))).reduce((a, b) => { // 按学期分组
           let semester = b.semester
           delete b.semester
           if (!a.length || a.slice(-1)[0].semester !== semester) {
@@ -61,8 +115,7 @@ exports.route = {
               = tr.children('td').toArray().map(k => $(k).text().trim())
 
             credit = parseFloat(credit)
-            score = `${score} (规${standardScore})`
-            return { semester, courseName, courseType: '', credit, score, scoreType }
+            return { semester, courseName, courseType: '', credit, score, standardScore, scoreType }
           })
         })
         .reduce((a, b) => a.concat(b), [])
