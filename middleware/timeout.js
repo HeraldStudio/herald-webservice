f36fc16980c7cd8277890a80832163ff3cc6b086
vhyme <vhyme@live.cn>
2018-05-22 20:51:53 +0800

    调试神器出炉

--- middleware/timeout.js
+++
@@ -1,9 +0,0 @@
-/**
-  # 超时中间件
-
-  为了防止某些特殊情况导致处理超时，在这里提供超时中间件，强制处理时间不超过20秒。
- */
-module.exports = async (ctx, next) => {
-  let timeout = new Promise((resolve, reject) => setTimeout(() => reject(504), 20000))
-  await Promise.race([next(), timeout])
-}
