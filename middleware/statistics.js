46ba5a236d035f2287f24e2b71de50433ed38de7
Rikumi <vhyme@live.cn>
2018-05-22 09:21:14 +0800

    stat数据库插入不脱离等待链

--- middleware/statistics.js
+++ middleware/statistics.js
@@ -5,17 +5,21 @@ module.exports = async (ctx, next) => {
   try {
     await next()
   } finally {
-    let end = +moment()
-    let time = start
-    let duration = end - start
-    let identity = ctx.user && ctx.user.isLogin ? ctx.user.identity : 'guest'
-    let platform = ctx.user && ctx.user.isLogin ? ctx.user.platform : 'guest'
-    let route = ctx.path
-    let method = ctx.method.toLowerCase()
+    try {
+      let end = +moment()
+      let time = start
+      let duration = end - start
+      let identity = ctx.user && ctx.user.isLogin ? ctx.user.identity : 'guest'
+      let platform = ctx.user && ctx.user.isLogin ? ctx.user.platform : 'guest'
+      let route = ctx.path
+      let method = ctx.method.toLowerCase()
 
-    // 考虑到某些情况（如重定向）时，返回中没有 JSON 格式的 body，只有 status
-    let status = ctx.body && ctx.body.code || ctx.status
-    let record = { time, identity, platform, route, method, status, duration }
-    /* await */ db.stat.insert(record)
+      // 考虑到某些情况（如重定向）时，返回中没有 JSON 格式的 body，只有 status
+      let status = ctx.body && ctx.body.code || ctx.status
+      let record = { time, identity, platform, route, method, status, duration }
+      await db.stat.insert(record)
+    } catch (e) {
+      console.error(e)
+    }
   }
 }
