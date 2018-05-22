90303d86994a75c8cd7e80d50a527def54388cfd
vhyme <vhyme@live.cn>
2018-05-22 17:24:36 +0800

    去除ws2 adapter一部分出错输出

--- middleware/adapter/unpacker.js
+++ middleware/adapter/unpacker.js
@@ -1,6 +1,14 @@
 module.exports = async (ctx, next) => {
   await next()
   if (ctx.path.indexOf('/adapter-') === 0 && typeof ctx.body === 'object') {
-    ctx.body = ctx.body.result || ctx.body.reason || 'OK'
+    let code = ctx.body.code || ctx.status
+    if (code < 400) {
+      ctx.body = ctx.body.result || 'OK'
+    } else {
+      ctx.body = {
+        code: 400,
+        reason: ctx.body.reason || 'error'
+      }
+    }
   }
 }
