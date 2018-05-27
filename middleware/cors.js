de11de1dedd7221b918964dad8da968095bb41c6
vhyme <vhyme@live.cn>
2018-05-27 12:48:20 +0800

    新增一些局域网允许域名

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,10 +1,12 @@
 // 允许的第三方前端域名，精确匹配
 const allowDomains = [
-  'myseu.cn',
-  'app.heraldstudio.com',
-  'www.heraldstudio.com',
-  'localhost',
-  '127.0.0.1'
+  /^myseu\.cn$/,
+  /^app\.heraldstudio\.com$/,
+  /^www\.heraldstudio\.com$/,
+  /^localhost$/,
+  /^127\./,
+  /^172\./,
+  /^192\./
 ]
 
 module.exports = async (ctx, next) => {
@@ -12,7 +14,7 @@ module.exports = async (ctx, next) => {
   if (origin) {
     // 不考虑端口和协议
     let domain = (origin.split('/').slice(-1)[0] || '').split(':')[0] || ''
-    if (domain && ~allowDomains.indexOf(domain)) {
+    if (domain && allowDomains.find(d => d.test(domain))) {
       ctx.set('Access-Control-Allow-Origin', origin)
       ctx.set('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,DELETE,PATCH')
       ctx.set('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,token')
