a54f22ae4a4e4f9bd9ee0e9ad2d15e7382550f4e
vhyme <vhyme@live.cn>
2018-05-29 19:04:18 +0800

    term实时更新

--- routes/api/term.js
+++ routes/api/term.js
@@ -1,16 +1,14 @@
 const { config } = require('../../app')
 
 exports.route = {
-  async get () {
-    return await this.publicCache('1d', async () => {
-      let now = +moment()
-      return Object.keys(config.term).map(k => {
-        let startMoment = moment(config.term[k], 'YYYY-M-D')
-        let startDate = +startMoment
-        let endDate = +startMoment.add(/-1$/.test(k) ? 4 : 18, 'weeks')
-        let current = startDate <= now && endDate > now
-        return { name: k, startDate, endDate, current }
-      }).sort((a, b) => a.startDate - b.startDate)
-    })
+  get () {
+    let now = +moment()
+    return Object.keys(config.term).map(k => {
+      let startMoment = moment(config.term[k], 'YYYY-M-D')
+      let startDate = +startMoment
+      let endDate = +startMoment.add(/-1$/.test(k) ? 4 : 18, 'weeks')
+      let current = startDate <= now && endDate > now
+      return { name: k, startDate, endDate, current }
+    }).sort((a, b) => a.startDate - b.startDate)
   }
 }
