0c4a8ab96925c6d404bdd18164b7ea067e47a57c
vhyme <vhyme@live.cn>
2018-06-25 17:27:38 +0800

    日志不再保留两年

--- database/stat.js
+++ database/stat.js
@@ -10,11 +10,11 @@ db.stat = {
   duration:   'int not null',
 }
 
-// 每次初始化时清理超过两年的日志
+// 每次初始化时清理超过两周的日志
 // 这里不能直接用 time 做条件，会导致遍历所有日志，导致启动时发生拥塞
-// 而是需要利用 time 的递增性，先查询第一个大于两年前的日志，然后删除所有这条日志之前的所有记录
+// 而是需要利用 time 的递增性，先查询第一个大于两周前的日志，然后删除所有这条日志之前的所有记录
 db`delete from stat where rowid < (
-  select rowid from stat where time > ${ +moment().subtract(2, 'years') } limit 1
+  select rowid from stat where time > ${ +moment().subtract(2, 'weeks') } limit 1
 );`
 
 module.exports = db
