136e9847682f3099a86d921144749aa6e456d99f
ThisTusooa <tusooa@vista.aero>
2018-07-09 15:04:27 +0800

    notice: Added default contentSelector

--- routes/api/notice/index.js
+++ routes/api/notice/index.js
@@ -181,7 +181,7 @@ exports.route = {
         absolute: true,
         baseUri: typeObj.baseUrl,
         inline: true
-      }).convert($(typeObj.contentSelector).html()).replace(/\*\*/g, ' ** ')
+      }).convert($(typeObj.contentSelector || 'body').html()).replace(/\*\*/g, ' ** ')
     } else if (nid) {
       let notice = await db.notice.find({ nid }, 1)
       return `# ${notice.title}\n\n${notice.content}`
