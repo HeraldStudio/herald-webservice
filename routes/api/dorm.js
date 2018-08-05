fcb655b8cb51c041c615bf3304a5a2ede0350552
rikumi <riku.mi@outlook.com>
2018-08-05 17:24:26 +0800

    加注释加注释

--- routes/api/dorm.js
+++ routes/api/dorm.js
@@ -3,6 +3,7 @@ const cheerio = require('cheerio')
 exports.route = {
   async get() {
     await this.useAuthCookie()
+    // 从总务处网站可以进入到这里，是一个三层框架套住的页面
     let res = await this.get(`http://hq.urp.seu.edu.cn/epstar/app/template.jsp?mainobj=SWMS/SSGLZXT/SSAP/V_SS_SSXXST&tfile=XSCKMB/BDTAG&filter=V_SS_SSXXST:XH='${this.user.cardnum}'`)
     let $ = cheerio.load(res.data)
     return {
