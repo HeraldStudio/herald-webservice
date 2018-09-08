a95b5f5308513b7ef607497aa81a992a6757ae25
Wolf-Tungsten <canis.tungsten@foxmail.com>
2018-09-08 11:13:27 +0800

    增加财务处接口

--- routes/api/cwc/auth.js
+++ routes/api/cwc/auth.js
@@ -1,7 +1,7 @@
 const cheerio = require('cheerio')
 const tough = require('tough-cookie')
 const qs = require('querystring')
-const FormData = require('form-data')
+
 exports.route = {
 
   /**
@@ -15,15 +15,43 @@ exports.route = {
     return { cookie, image }
   },
 
+  /**
+  * POST /api/cwc/auth
+  * 进行登录操作，返回token
+  **/
   async post({uid, pwd, chkcode, cookie}) {
-    console.log(qs.stringify({ uid, pwd, chkcode }))
     cookie = cookie[0].split(';')[0]
     let headers = {cookie}
-    console.log(cookie)
     let res = await this.post('http://caiwuchu.seu.edu.cn/payment/loginnewAction.action',qs.stringify({uid, pwd, chkcode}), {headers})
-    if (typeof(res.data) === 'Buffer') {
-      // 登录成功
-      cookie = 
+    
+    if (res.data === 'uierror') {
+      throw '用户名或密码错误'
     }
+
+    if (res.data === 'chkcodeerror') {
+      throw '验证码填写错误'
+    }
+    
+    if (typeof(res.data) === 'object'){
+      return {cookie}
+    }
+    console.log(typeof(res.data))
+    console.log(res.data)
+    throw '与财务处网站通讯出现故障'
+  },
+
+  async put({cardnum, idnum}) {
+    let req = {
+      'userinfo.userid':cardnum,
+      'userinfo.idcardno':idnum
+    }
+    let res = await this.post('http://caiwuchu.seu.edu.cn/payment/login_resetPwd.action', qs.stringify(req))
+    if (res.data == '1') {
+      return '密码已重置'
+    }
+    if (res.data == '2') {
+      throw '一卡通号或者身份证号不正确'
+    }
+    throw '与财务处网站通信出现故障'
   }
 }
