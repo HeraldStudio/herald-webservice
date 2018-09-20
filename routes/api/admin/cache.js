46175547ca3fddbc67a65d055c628fce65563d16
Wolf-Tungsten <canis.tungsten@foxmail.com>
2018-09-20 14:34:52 +0800

    添加管理员清理cache

---
+++ routes/api/admin/cache.js
@@ -0,0 +1,21 @@
+// 管理员清理cache的请求
+
+exports.route = {
+  async get () {
+    if (!this.admin || !this.admin.maintenance) {
+      throw 403
+    }
+    throw 'Delete ONLY'
+  },
+  async delete ({ cacheName }) {
+    console.log(admin)
+    if (!this.admin || !this.admin.maintenance) {
+      throw 403
+    }
+    if (!cacheName) {
+      throw '只允许清理指定路由'
+    }
+    await this.clearAllCache(cacheName)
+    return 'OK'
+  }
+}
