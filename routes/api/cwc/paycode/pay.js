5c8708dc9f92295653c7304cd8566581c6fed149
Wolf-Tungsten <canis.tungsten@foxmail.com>
2018-09-24 22:14:06 +0800

    添加付款接口

--- routes/api/cwc/paycode/pay.js
+++ routes/api/cwc/paycode/pay.js
@@ -9,6 +9,22 @@ exports.route = {
   * @apiParam pwd 使用支付密码登录支付平台，获取缴费信息
   **/
   async get({ pwd }) {
-    return 'qnmd'
+    let res = await this.get(`http://payment.seu.edu.cn/pay/dealPay.html?pwd=${pwd}`)
+    let keys = ['projectName', 'rIdExt', 'currencyTypeShow','amount','pwd']
+    let payInfo = {
+      'projectName': /"projectName":"([0-9A-Za-z\u4e00-\u9fa5\s]+)"/im.exec(res.data),
+      'rIdExt': /"rIdExt":"([0-9A-Za-z]+)"/im.exec(res.data),
+      'currencyTypeShow': /"currencyTypeShow":"([0-9A-Za-z\u4e00-\u9fa5\[\]]+)"/im.exec(res.data),
+      'amount': /"amount":"([0-9\.]+)"/im.exec(res.data),
+      'userName': /"userName":"([A-Za-z\u4e00-\u9fa5\s]+)"/im.exec(res.data),
+      'pwd': /\{"pwd":"([0-9A-Z]+)"\}/im.exec(res.data)
+    }
+    Object.keys(payInfo).forEach(k => {
+      payInfo[k] = payInfo[k] ? payInfo[k][1] : '未知'
+    })
+    console.log(payInfo)
+    payInfo.amount = parseFloat(payInfo.amount) ? parseFloat(payInfo.amount) : 0
+    payInfo.url = `http://payment.seu.edu.cn/pay/deal_CCB_union.html?autoSubmit=Y&realPay=1&pwd=${payInfo.pwd}`
+    return payInfo
   }
 }
