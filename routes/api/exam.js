1908d95a1c3788dad281e16e9d59c1ec8b72cc48
Wolf-Tungsten <canis.tungsten@foxmail.com>
2018-11-23 16:12:58 +0800

    考试时间兼容18级考试查询

--- routes/api/exam.js
+++ routes/api/exam.js
@@ -8,14 +8,64 @@ exports.route = {
   **/
 
   async get() {
-    return await this.userCache('1h+', async () => {
+    return await this.userCache('10m+', async () => {
+
+      let { name, cardnum } = this.user
+
+      // 新考试安排系统-目前使用18级本科生数据进行测试
+      if (/^21318/.test(cardnum)) { 
+        // 1. 需要使用 ids6 认证
+        await this.useAuthCookie({ ids6: true })
+
+        // 2. 获取下一步操作所需的 URL
+        const urlRes = await this.get('http://ehall.seu.edu.cn/appMultiGroupEntranceList?appId=4768687067472349&r_t=' + Date.now())
+
+        let url = '';
+        urlRes.data && urlRes.data.data && urlRes.data.data.groupList && urlRes.data.data.groupList[0] &&
+        (url = urlRes.data.data.groupList[0].targetUrl);
+        if (!url)
+          throw 400;
+
+        // 3. 访问一下上述 URL ，获取名为 _WEU 的 cookie
+        await this.get(url)
+
+        // 4. 获取学期代号
+        let termRes = await this.post('http://ehall.seu.edu.cn/jwapp/sys/studentWdksapApp/modules/wdksap/dqxnxq.do')
+        let termCode = termRes.data.datas.dqxnxq.rows[0].DM
+
+        // 5. 获取原始的考试安排数据
+        let examData = await this.post('http://ehall.seu.edu.cn/jwapp/sys/studentWdksapApp/modules/wdksap/wdksap.do', 
+                                      {XNXQDM: termCode,
+                                      '*order':' -KSRQ,-KSSJMS'})
+        examData = examData.data.datas.wdksap.rows
+        let examList = examData.map( k => {
+          // 分析时间
+          let rawTime = k.KSSJMS
+          rawTime = rawTime.split('(')[0]
+          let date = rawTime.split(' ')[0]
+          let [startTime, endTime] = rawTime.split(' ')[1].split('-')
+          startTime = +moment(date+'-'+startTime, 'YYYY-MM-DD-HH:mm')
+          endTime = +moment(date+'-'+endTime, 'YYYY-MM-DD-HH:mm')
+          duration = (endTime - startTime) / 1000 / 60
+          return {
+            startTime,endTime,duration,
+            semester:k.XNXQDM,
+            campus:'-',
+            courseName:k.KCM,
+            courseType:k.KSMC,
+            teacherName:k.ZJJSXM,
+            location:k.JASMC
+          }
+        })
+
+        return examList
+      }
 
       // 先检查可用性，不可用直接抛异常或取缓存
       this.guard('http://xk.urp.seu.edu.cn/studentService/system/showLogin.action')
 
       await this.useAuthCookie()
       // 经测试，使用老门户的 cookie，并不需要再登录教务处。
-      let { name, cardnum } = this.user
       res = await this.get(
         'http://xk.urp.seu.edu.cn/studentService/cs/stuServe/runQueryExamPlanAction.action'
       )
