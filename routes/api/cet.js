89d37d4ec41e7eda26c569b430843dadfcfa4fd2
Wolf-Tungsten <canis.tungsten@foxmail.com>
2018-12-15 12:03:47 +0800

    调整cet查询返回格式

--- routes/api/cet.js
+++ routes/api/cet.js
@@ -15,14 +15,19 @@ exports.route = {
             let res = await this.post('http://ehall.seu.edu.cn/publicapp/sys/sljzkzdy/modules/ksxxhd/T_KSBM_ZKZDY_QUERY.do', { XH: cardnum })
             if (res.data && res.data.datas.T_KSBM_ZKZDY_QUERY.rows && res.data.datas.T_KSBM_ZKZDY_QUERY.rows[0]) {
                 // 存在查询信息，更新数据库记录
-                await cetCollection.deleteMany({XH:cardnum})
+                await cetCollection.deleteMany({cardnum})
                 let cetInfo = res.data.datas.T_KSBM_ZKZDY_QUERY.rows[0]
+                let {ZKZH:ticketNo, KSSJ:examTime, KSDD:location, KCH:roomNo,
+                ZWH:seatNo,KSXM_DISPLAY:examType} = cetInfo
+                examTime = +moment(examTime, 'YYYY年M月D日hh点mm分')
+                cetInfo = {cardnum, ticketNo, examTime, location, roomNo, seatNo, examType}
                 await cetCollection.insertOne(cetInfo)
                 return cetInfo
             } else {
                 // 官网不存在，从数据库查询
-                let cetInfo = await cetCollection.findOne({XH:cardnum})
+                let cetInfo = await cetCollection.findOne({cardnum})
                 if (cetInfo) {
+                    cetInfo.store=true
                     return cetInfo
                 }
                 // 数据库没有
