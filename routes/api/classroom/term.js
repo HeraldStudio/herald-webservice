0677d12b9338434dd2cb8ae889df6d47c4c95f1d
Wolf-Tungsten <canis.tungsten@foxmail.com>
2019-03-11 18:38:07 +0800

    空教室查询接口

---
+++ routes/api/classroom/term.js
@@ -0,0 +1,8 @@
+exports.route = {
+    // 获取所有的可以查询的校区
+    async get () {
+        let now = +moment()
+        let rawData = await this.get(`http://58.192.114.179/classroom/common/gettermlistex?_=${now}`)
+        return rawData.data
+    }
+}
