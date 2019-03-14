1764a8f7792cc7f2ce630e307bc61c96e279c458
Wolf-Tungsten <canis.tungsten@foxmail.com>
2019-03-14 09:59:38 +0800

    调试缓存删除

--- routes/api/admin/cache.js
+++ routes/api/admin/cache.js
@@ -14,7 +14,12 @@ exports.route = {
     if (!cacheName) {
       throw '只允许清理指定路由'
     }
-    await this.clearAllCache(cacheName)
+    try{
+      await this.clearAllCache(cacheName)
+    } catch(e) {
+      console.log(e.message)
+    }
+    
     return 'OK'
   }
 }
