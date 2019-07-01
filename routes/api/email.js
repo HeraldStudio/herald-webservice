33bf1d546f63e9ed9a51f5dc22181320a4d58012
ZZJ <zhaozjseu@163.com>
2019-07-01 17:44:27 +0800

    未读邮件查询

---
+++ routes/api/email.js
@@ -0,0 +1,50 @@
+const imaps = require('imap-simple');
+
+
+
+
+exports.route = {
+    async get(){
+       let config = {
+            imap: {
+                user: this.user['cardnum']+'@seu.edu.cn',
+                password: this.user['password'],
+                host: 'imap.seu.edu.cn',
+                port: 993,
+                tls: true,
+                authTimeout: 3000,
+                tlsOptions: {
+                    rejectUnauthorized: false
+                }
+            }
+        };
+
+        let connection = await imaps.connect(config)
+        await connection.openBox('INBOX')
+        
+        //搜索条件
+        var searchCriteria = [
+            'UNSEEN'
+        ];
+        var fetchOptions = {
+            bodies: ['HEADER', 'TEXT'],
+            markSeen: false
+        };
+
+        let results = await connection.search(searchCriteria, fetchOptions)
+
+        let subjects = results.map(function (res) {
+            return res.parts.filter(function (part) {
+                return part.which === 'HEADER';
+            })[0].body.subject[0];
+        });
+        subjects = subjects.map(subject => {
+
+        return (subject === '') ? '无主题邮件' : subject
+
+        })
+
+        return subjects
+
+    }
+}
