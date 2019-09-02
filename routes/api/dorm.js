b4b1945da1edaafab72ea41c62daa0c82cf7b3b0
Wolf-Tungsten <canis.tungsten@foxmail.com>
2019-09-02 13:16:42 +0800

    限制只允许本科生查宿舍（xswl

--- routes/api/dorm.js
+++ routes/api/dorm.js
@@ -5,6 +5,9 @@ exports.route = {
     await this.useAuthCookie()
     let res = await this.get('http://my.seu.edu.cn/pnull.portal?action=showItem&.ia=false&.pen=pe562&itemId=231&childId=240&page=1')
     let { name, cardnum } = this.user
+    if(!cardnum.startsWith('213')){
+      throw '只允许本科生查询'
+    }
     let $ = cheerio.load(res.data)
     this.logMsg = `${name} (${cardnum}) - 查询宿舍信息`
     let data = $($($('#sb_table').children()[0]).children()[1]).children().map(function(){
