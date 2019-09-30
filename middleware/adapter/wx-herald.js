294ba5559bb1774fe77a5b5cbf748973a26a61c0
ZZJ <zhaozjseu@163.com>
2019-09-30 12:15:29 +0800

    继续改 bug

--- middleware/adapter/wx-herald.js
+++ middleware/adapter/wx-herald.js
@@ -67,7 +67,7 @@ const handler = {
 
   async '手机卡'() {
     let form = {
-      media: fs.createReadStream('../../static/ad.jpeg')
+      media: fs.createReadStream('./static/ad.jpeg')
     }
     api.post(`/media/upload?access_token=${accessToken}&type=image`, form).then(res => {
       console.log(res)
