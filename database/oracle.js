de80d608019d28927e4b015dfb364350a2e6aebc
wolf-tungsten <canis.tungsten@foxmail.com>
2020-01-20 21:39:09 +0800

    添加一卡通中间库

--- database/oracle.js
+++ database/oracle.js
@@ -3,14 +3,23 @@ const dbSecret = require('./oracle-secret.js')
 
 oracledb.autoCommit = true
 let connectionPool = null
+let allinoneCardConnectionPool = null
 
 module.exports = {
   async getConnection() {
     if(!connectionPool){
       connectionPool = await oracledb.createPool({ 
-        ...dbSecret
+        ...dbSecret.herald
       })
     }
     return await connectionPool.getConnection()
+  },
+  async getAllinoneCardConnection() {
+    if(!allinoneCardConnectionPool){
+      allinoneCardConnectionPool = await oracledb.createPool({ 
+        ...dbSecret.allinoneCard
+      })
+    }
+    return await allinoneCardConnectionPool.getConnection()
   }
 }
