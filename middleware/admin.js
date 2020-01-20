cb080ad6c1a84c7c8ddce399bad913818d8ab375
wolf-tungsten <canis.tungsten@foxmail.com>
2020-01-20 18:18:25 +0800

    升级 admin 中间件

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,89 +1,39 @@
-const crypto = require('crypto')
-const mongodb = require('../database/mongodb')
-
-//数据库迁移代码
-// (async() => {
-//   console.log('正在迁移admin数据库')
-//   let allAdmins = await db.admin.find({}, -1)
-//   let adminCollection = await mongodb('herald_admin')
-//   if (allAdmins.length > 0) {
-//   await adminCollection.insertMany(allAdmins)
-//   }
-//   console.log(`共迁移${allAdmins.length}条记录`)
-// })();
-// (async() => {
-//   console.log('正在迁移domain数据库')
-//   let allDomains = await db.domain.find({}, -1)
-//   let domainCollection = await mongodb('herald_admin_domain')
-//   if (allDomains.length > 0) {
-//   await domainCollection.insertMany(allDomains)
-//   }
-//   console.log(`共迁移${allDomains.length}条记录`)
-// })();
-
-// 程序启动时，生成超级管理员 Token
-// 为了防止与普通用户碰撞，此处字节数跟普通用户 token 字节数做区分，切勿轻易改成跟普通用户长度相同，否则会有问题
-const superToken = Buffer.from(crypto.randomBytes(16)).toString('hex')
-console.log('本次会话的超级管理员 Token 为：' + chalkColored.blue(superToken) + '，请妥善保管')
-
-// ctx.admin API
-// 对于非管理员，ctx.admin 为 null
-// 对于普通管理员，ctx.admin 为包含所有自己所属的管理员域键为成真值的对象
-// 对于超级管理员，ctx.admin 为包含所有管理员域键为成真值的对象，且 super 键也为 true
+/**
+ * # 管理员权限中间件
+ * 管理员身份表 H_ADMIN
+ * 管理员权限表 H_ADMIN_PERMISSION
+ * 
+ * 暴露 ctx.hasPermission 接口，当需要使用权限时，直接传入所需权限的
+ * 枚举值名称，若不具有对应权限就抛 401
+ * 
+ */
 module.exports = async (ctx, next) => {
-  let adminCollection = await mongodb('herald_admin')
-  let domainCollection = await mongodb('herald_admin_domain')
-  // 中间件处理，允许下游查询当前用户的权限
-  ctx.admin = null
-  if (ctx.request.headers.token === superToken) {
-    ctx.admin = { super: true }
-    let domains = await domainCollection.find().toArray()
-    for (let domain of domains) {
-      ctx.admin[domain.domain] = {
-        domain: domain.name,
-        desc: domain.desc,
-        level: 0
-      }
-    }
-  } else if (ctx.request.headers.token
-    && ctx.request.headers.token.length === superToken.length) {
-    // 若 token 与超管 token 长度相同但不是超管 token，认为是老超管登录过期
-    ctx.throw(401)
-  } else if (ctx.user.isLogin) {
-    let { cardnum } = ctx.user
-    //let admins = await db.admin.find({ cardnum })
-    let admins = await adminCollection.find({ cardnum }).toArray()
-    for (let admin of admins) {
-      if (!ctx.admin) {
-        ctx.admin = {}
-      }
-      let domain = admin.domain
-      //let domainInfo = await db.domain.find({ domain }, 1)
-      let domainInfo = await domainCollection.findOne({ domain })
-      admin.domain = domainInfo.name
-      admin.desc = domainInfo.desc
-      ctx.admin[domain] = admin
-    }
 
-    // 利用 Proxy 机制，每当 get 某个 domain 权限时，自动更新数据库中的调用时间
-    if (ctx.admin) {
-      ctx.admin = new Proxy(ctx.admin, {
-        set(target, key, value) {
-          target[key] = value
-          return true
-        },
-        get(target, key) {
-          if (target[key]) {
-            let now = +moment()
-            let domain = key
-            //db.admin.update({ cardnum, domain }, { lastUsed: now })
-            adminCollection.updateOne({ cardnum, domain }, { $set:{ lastUsed: now } })
-          }
-          return target[key]
-        }
-      })
+  // 中间件处理，允许下游查询当前用户的权限
+  ctx.hasPermission = async ( permissionName ) => {
+    if(!ctx.user.isLogin) {
+      // 还没登录就访问？直接给你401
+      throw 401
+    }
+    let { cardnum } = ctx.user
+    let permissions = await ctx.db.execute(`
+      SELECT p.PERMISSION 
+      FROM TOMMY.H_ADMIN a, TOMMY.H_ADMIN_PERMISSION p 
+      WHERE a.CARDNUM = b.CARDNUM AND a.CARDNUM = :cardnum`,
+    { cardnum })
+
+    permissions = permissions.rows.map(p => p[0])
+    if(permissions.indexOf(permissionName) !== -1) {
+      let now = moment()
+      ctx.db.execute(`
+              UPDATE TOMMY.H_ADMIN 
+              SET LAST_INVOKED_TIME = :lastInvokedTime
+              WHERE CARDNUM = :cardnum
+              `, { lastInvokedTime: now.toDate(), cardnum })
+      return true
+    } else {
+      throw 401
     }
   }
-
   await next()
 }
