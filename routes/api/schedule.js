723b7281e117f16ebd9a551a6a0c160f90fb4326
ZZJ <zhaozjseu@163.com>
2020-01-22 23:08:04 +0800

    /api/schedule加限制条件

--- routes/api/schedule.js
+++ routes/api/schedule.js
@@ -5,6 +5,10 @@ exports.route = {
    * 获取校历（图片地址）
    */
   async get() {
+    if (!this.user.isLogin){
+      // 不登录都给我爬
+      throw 403
+    }
     // 将校历图片上传到outline服务器，直接图片连接
     return 'https://static.myseu.cn/schedule_2019.jpg'
   }
