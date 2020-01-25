78b358b5bc2ca54a12f52e9bfbf9d22f586c303b
R-shrug <656598766@qq.com>
2020-01-25 15:13:25 +0800

    修改了考试获取的接口

--- routes/api/exam.js
+++ routes/api/exam.js
@@ -10,60 +10,63 @@ exports.route = {
 
   async get() {
 
-    let { name, cardnum, schoolnum } = this.user
+    let { cardnum } = this.user
 
     let now = +moment()
 
     // 获取考试数据
     let record = await this.db.execute(`
-    select  g.XNXQDM,g.MC,T_KC_KCB.KCM,T_JZG_JBXX.XM,g.KSSJMS,JASMC, KSSC
-    from T_JZG_JBXX,T_KC_KCB,(
-      select   T_RW_JSB.JSH,f.MC, f.JASMC, f.KSRWID, f.KSSC, f.KSSJMS, f.XNXQDM, f.KCH, f.JXBID
-      from T_RW_JSB,(
-        select T_ZXBZ_XXXQ.MC,e.JASMC, KSRWID, KSSC, KSSJMS, XNXQDM, KCH,JXBID
-        from T_ZXBZ_XXXQ,(
-          select  d.JASMC, d.KSRWID, d.KSSC, d.KSSJMS, d.XNXQDM,T_KW_KSRW.KCH,XXXQDM,JXBID
-          from T_KW_KSRW,(
-            select T_JAS_JBXX.JASMC,c.KSRWID, KSSC, KSSJMS, XNXQDM
-            from T_JAS_JBXX,(
-              select T_KW_KSAPDD.JASDM,b.KSRWID,b.KSSC,b.KSSJMS,b.XNXQDM
-              from T_KW_KSAPDD,(
-                select T_KW_KSAPSJ.XNXQDM,T_KW_KSAPSJ.KSSJMS,T_KW_KSAPSJ.KSSC,T_KW_KSAPSJ.KSRWID,a.KSAPBH
-                from T_KW_KSAPSJ,(
-                  select T_KW_KSAPXS.KSRWID,T_KW_KSAPXS.KSAPBH
-                  from T_KW_KSAPXS
-                  where T_KW_KSAPXS.xh=:cardnum)a
-                where a.KSRWID=T_KW_KSAPSJ.KSRWID)b
-              where T_KW_KSAPDD.KSRWID=b.KSRWID and T_KW_KSAPDD.KSAPBH=b.KSAPBH)c
-            where T_JAS_JBXX.JASDM=c.JASDM)d
-          where d.KSRWID=T_KW_KSRW.KSRWID)e
-        where T_ZXBZ_XXXQ.DM=e.XXXQDM)f
-      where T_RW_JSB.JXBID=f.JXBID and T_RW_JSB.KCH=f.KCH)g
-    where T_JZG_JBXX.ZGH=g.JSH and g.KCH=T_KC_KCB.KCH
+    select h.XNXQDM, MC, KCM, T_KW_KSPC.KSMC, h.XM, KSSJMS, JASMC, KSSC
+    from T_KW_KSPC,(
+      select  g.XNXQDM,g.MC,T_KC_KCB.KCM,T_JZG_JBXX.XM,g.KSSJMS,JASMC, KSSC, g. KSDM
+      from T_JZG_JBXX,T_KC_KCB,(
+        select   T_RW_JSB.JSH,f.MC, f.JASMC, f.KSRWID, f.KSSC, f.KSSJMS, f.XNXQDM, f.KCH, f.JXBID, f.KSDM
+        from T_RW_JSB,(
+          select T_ZXBZ_XXXQ.MC,e.JASMC, KSRWID, KSSC, KSSJMS, XNXQDM, KCH, JXBID, KSDM
+          from T_ZXBZ_XXXQ,(
+            select  d.JASMC, d.KSRWID, d.KSSC, d.KSSJMS, d.XNXQDM,T_KW_KSRW.KCH,XXXQDM,JXBID,KSDM
+            from T_KW_KSRW,(
+              select T_JAS_JBXX.JASMC,c.KSRWID, KSSC, KSSJMS, XNXQDM
+              from T_JAS_JBXX,(
+                select T_KW_KSAPDD.JASDM,b.KSRWID,b.KSSC,b.KSSJMS,b.XNXQDM
+                from T_KW_KSAPDD,(
+                  select T_KW_KSAPSJ.XNXQDM,T_KW_KSAPSJ.KSSJMS,T_KW_KSAPSJ.KSSC,T_KW_KSAPSJ.KSRWID,a.KSAPBH
+                  from T_KW_KSAPSJ,(
+                    select T_KW_KSAPXS.KSRWID,T_KW_KSAPXS.KSAPBH
+                    from T_KW_KSAPXS
+                    where T_KW_KSAPXS.xh=:cardnum)a
+                  where a.KSRWID=T_KW_KSAPSJ.KSRWID)b
+                where T_KW_KSAPDD.KSRWID=b.KSRWID and T_KW_KSAPDD.KSAPBH=b.KSAPBH)c
+              where T_JAS_JBXX.JASDM=c.JASDM)d
+            where d.KSRWID=T_KW_KSRW.KSRWID)e
+          where T_ZXBZ_XXXQ.DM=e.XXXQDM)f
+        where T_RW_JSB.JXBID=f.JXBID and T_RW_JSB.KCH=f.KCH)g
+      where T_JZG_JBXX.ZGH=g.JSH and g.KCH=T_KC_KCB.KCH)h
+    where T_KW_KSPC.KSDM = h.KSDM
     `,[cardnum])
-
-    let result = record.rows.map( Element => {
-      let [semester, campus, courseName, teacherName, time, location, duration]=Element
+    let result = record.rows.map(Element => {
+      let [semester, campus, courseName, courseType, teacherName, time, location, duration] = Element
+      console.log(Element)
       let startMoment = moment(time, 'YYYY-MM-DD HH:mm(dddd)')
       let startTime = +startMoment
       let endTime = +startMoment.add(duration, 'minutes')
-
+      courseName = courseName + ' ' + courseType.split(' ')[1]
       return { semester, campus, courseName, teacherName, startTime, endTime, location, duration }
     })
 
     // 获取自定义考试数据
-    let  customExam= await this.db.execute(`
+    let customExam = await this.db.execute(`
       select semester, campus, courseName, teacherName, startTime, endTime, location, duration
       from t_my_exam
       where cardnum=:cardnum
-    `,[cardnum])
+    `, [cardnum])
 
-    customExam.rows.map( Element => {
-      let [semester, campus, courseName, teacherName, startTime, endTime, location, duration]=Element
+    customExam.rows.map(Element => {
+      let [semester, campus, courseName, teacherName, startTime, endTime, location, duration] = Element
       result.push({ semester, campus, courseName, teacherName, startTime, endTime, location, duration })
     })
-    
-    result.filter( e => e.endTime > now)// 防止个别考生考试开始了还没找到考场🤔
+
+    // result.filter( e => e.endTime > now)// 防止个别考生考试开始了还没找到考场🤔
     return result
   },
 
@@ -79,7 +82,7 @@ exports.route = {
   * @apiParam duration    考试时长   单位：分
   **/
 
-  async post({semester, campus, courseName, teacherName, startTime, location, duration}) {
+  async post({ semester, campus, courseName, teacherName, startTime, location, duration }) {
 
     let { cardnum } = this.user
 
@@ -111,9 +114,9 @@ exports.route = {
 
     result = await this.db.executeMany(sql, binds, options);
 
-    if(result.rowsAffected>0){
+    if (result.rowsAffected > 0) {
       return '自定义考试成功'
-    }else{
+    } else {
       throw '自定义考试失败'
     }
   }
