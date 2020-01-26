8c1e68af29eba4d58726ecf1a9619a305c6cc809
ZZJ <zhaozjseu@163.com>
2020-01-26 17:22:56 +0800

    修改/api/pe/exam

--- routes/api/pe/exam.js
+++ routes/api/pe/exam.js
@@ -1,18 +1,29 @@
 
+/**
+ * 体育理论考试的参考，注意是参考哦！！！
+ * 
+ * 暂时不使用缓存
+ * 
+ * 这个网站是怎么得到我也很懵逼，暂且先这样用吧
+ */
 exports.route = {
   async get() {
-    return await this.publicCache('1mo', async () => {
-      // 暂时固定课程号为 2598，如果日后有更改可以取消注释这一段
-      // let res = await this.get('http://tyx.seu.edu.cn')
-      // let $ = cheerio.load(res.data)
-      // let cid = /course_(\d+)/.exec($('a[title="国家级资源共享课程"]').attr('href'))[1]
-      
-      let cid = 2598
-      let res = await this.get(`http://mobile.icourses.cn/hep-mobile/sword/app/share/detail/getExam?courseId=${cid}&subjectType=1`)
-      return res.data.data.map(k => k.resList[0]).map(k => ({
-        title: k.title,
-        url: k.fullResUrl
-      })).filter(k => k.title !== '身体素质')
-    })
+
+    // 暂时固定课程号为 2598，如果日后有更改可以取消注释这一段
+    // let res = await this.get('http://tyx.seu.edu.cn')
+    // let $ = cheerio.load(res.data)
+    // let cid = /course_(\d+)/.exec($('a[title="国家级资源共享课程"]').attr('href'))[1]
+    let cid = 2598
+    console.log(`http://mobile.icourses.cn/hep-mobile/sword/app/share/detail/getExam?courseId=${cid}&subjectType=1`)
+    let res = await this.get(`http://mobile.icourses.cn/hep-mobile/sword/app/share/detail/getExam?courseId=${cid}&subjectType=1`)
+    // buffer 转为JSON
+    res = JSON.parse(res.data.toString()).data
+    // 获取真正的数据
+    res = res.map( k => k.resList[0]).map( k => ({
+      title: k.title,
+      url: k.fullResUrl
+    }))
+
+    return res
   }
 }
