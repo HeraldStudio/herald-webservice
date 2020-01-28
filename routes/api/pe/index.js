7aa280071f0b52ed61e97366214b55a4180362e8
ZZJ <zhaozjseu@163.com>
2020-01-28 17:41:20 +0800

    pe查询接口

--- routes/api/pe/index.js
+++ routes/api/pe/index.js
@@ -1,7 +1,15 @@
-const cheerio = require('cheerio')
+// const cheerio = require('cheerio')
+const peConfig = require('../../../sdk/pe.json')
+const axios = require('axios')
+const sha = require('sha1')
+const moment = require ('moment')
 
 const hintTable = [
-  '小猴提醒：大一起床不抓紧，大四跑操两行泪'
+  '小猴提醒：起床不抓紧，跑操两行泪',      // 学期初提醒
+  '小猴提醒：不错哦～要继续保持～',      
+  '小猴提醒：要抓紧跑操哦～',             
+  '小猴叹息：小猴为你的跑操感到悲哀',      // 彻底跑不完了
+  '小猴祝贺：恭喜你已经完成了跑操任务🎉'   // 完成跑操任务
 ]
 
 exports.route = {
@@ -10,72 +18,104 @@ exports.route = {
   * GET /api/pe
   * 跑操查询
   * 暂时先不使用缓存
+  * @apiReturn { count, detail, health, remainDays, hint }
   **/
   async get() {
+
+    if (!this.user.isLogin){
+      throw 403
+    }
+    const cardnum = this.user.cardnum
+    const now = +moment()
+    // 获取体测成绩
+    let signature = sha(`ak=${peConfig.ak}&cardnum=${cardnum}&nounce=healthScore&sk=${peConfig.sk}`)
+    const healthScoreUrl = peConfig.url + '/healthScore?' +`ak=${peConfig.ak}&cardnum=${cardnum}&nounce=healthScore&signature=${signature}`
+    let res = await axios.get(healthScoreUrl)
+
+    let health = {
+      "身高":res.data.stature,
+      "体重":res.data.avoirdupois,
+      "肺活量":res.data.vitalCapacity,
+      "肺活量分数":res.data.vitalCapacityScore,
+      "肺活量评价":res.data.vitalCapacityConclusion,
+      "50米":res.data.fiftyMeter,
+      "50米分数":res.data.fiftyMeterScore,
+      "50米评价":res.data.fiftyMeterConclusion,
+      "立定跳远":res.data.standingLongJump,
+      "立定跳远分数":res.data.standingLongJumpScore,
+      "立定跳远评价":res.data.standingLongJumpConclusion,
+      "BMI值":res.data.BMI,
+      "BMI分数":res.data.BMIScore,
+      "BMI评价":res.data.BMIConclusion,
+      "坐体前屈":res.data.bend,
+      "坐体前屈分数":res.data.bendScore,
+      "坐体前屈评价":res.data.bendConclusion,
+      "总分":res.data.score
+    }
+    if (res.data.sex === '男') {
+      health["1000米"] = res.data.kiloMeter
+      health["1000米分数"] = res.data.kiloMeterScore
+      health["1000米评价"] = res.data.kiloMeterConclusion
+      health["引体向上"] = res.data.lie
+      health["引体向上分数"] = res.data.lieScore
+      health["引体向上评价"] = res.data.lieConclusion
+    } else {
+      health["800米"] = res.data.kiloMeter
+      health["800米分数"] = res.data.kiloMeterScore
+      health["800米评价"] = res.data.kiloMeterConclusion
+      health["仰卧起坐"] = res.data.lie
+      health["仰卧起坐分数"] = res.data.lieScore
+      health["仰卧起坐评价"] = res.data.lieConclusion
+    }
+
+    // console.log(health)
     
+    // 获取跑操数据
+    signature = sha(`ak=${peConfig.ak}&cardnum=${cardnum}&nounce=morningExercises&sk=${peConfig.sk}`)
+    const morningExercisesUrl = peConfig.url + '/morningExercises?' +`ak=${peConfig.ak}&cardnum=${cardnum}&nounce=morningExercises&signature=${signature}`
+    res = await axios.get(morningExercisesUrl)
+    
+    // 过滤，仅获取当前学期的的跑操次数
+    // 测试期间，暂不过滤
+    // res.data.records = res.data.records.filter( 
+    //   k => +moment(+k) > this.term.currentTerm.startDate && +moment(+k) < this.term.currentTerm.endDate
+    // )
 
-    // 取统一身份认证 Cookie，登录早操查询网站，拿到网站 Session Cookie
+    //console.log(res.data.records)
+    
+    const count = res.data.records.length
 
-    // 为了加快速度，预先并行获取跑操次数、跑操详情、体测成绩三个页面
-    let { cardnum } = this.user
-    let [countHtml, detailHtml, healthHtml] = await Promise.all([
-      'http://zccx.seu.edu.cn/student/queryCheckInfo.jsp',
-      'http://zccx.seu.edu.cn/SportWeb/gym/gymExercise/gymExercise_query_result_2.jsp?xh=' + cardnum,
-      'http://zccx.seu.edu.cn/student/queryHealthInfo.jsp'
-    ].map(async k => {
-      return (await this.get(k)).data
-    }))
-
-    // 计算跑操次数
-    let $ = cheerio.load(countHtml)
-    let count = $('td + td[bgcolor]').toArray().map(k => {
-      let text = $(k).text()
-      return parseInt(/\d+/.exec(text)[0])
-    }).reduce((a, b) => a + b, 0)
-
-    // 计算跑操详情（每次打卡时间戳）
-    $ = cheerio.load(detailHtml)
-    let detail = $('tr[height="30"]').toArray().map(k => {
-      let [date, time] = $(k).children('td').toArray().map(k => $(k).text()).slice(3)
-      let [hour, min] = /^\d+\.\d{2}/.exec(time + '0')[0].split('.').map(k => parseInt(k))
-      return +moment(date).hour(hour).minute(min)
-    })
-
-    // 次数偶尔刷不出来，误显示为零，这种情况下暂时用详情的条数代替次数
-    count = Math.max(count, detail.length)
-
-    // 计算体测成绩
-    $ = cheerio.load(healthHtml)
-    let health = $('tr[height="30"]').toArray().map(k => {
-      let [name, value, score, grade] = $(k).children('td').toArray().map(k => $(k).text().trim())
-      value = parseFloat(value)
-      score = parseFloat(score)
-      return { name, value, score, grade }
-    })
-
-    //对于 17 16 级即使跑操的数据统计为0 也不算上游数据失效
-    if (count === 0 && !/2131[567]\d{4}/.test(this.user.cardnum)) {
-      throw '上游数据失效'
+    // 计算跑操剩余天数
+    // 默认跑操时间前16周 
+    const beginOfTerm = this.term.currentTerm.startDate
+    const remainDays = Array(16 * 7).fill()
+    // 当前学期每一天的跑操结束时间戳
+    // 注意这里要克隆一次，不能在原对象上直接操作
+      .map((_, i) => +(moment(beginOfTerm).clone().add(i, 'days').hour(7).minute(20)))
+    // 过滤掉时间比现在晚的
+      .filter(k => now < k)
+    // 时间戳变为星期
+      .map(k => moment(k).day())
+    // 过滤掉周末
+      .filter(k => k >= 1 && k<= 5)
+      .length
+    // console.log(remainDays)
+    
+    let hint
+    if ( now < +(moment(this.term.currentTerm.startDate).add(45,'day'))) {
+      // 开学一个月之前，显示提醒用语
+      hint = hintTable[0]
+    } else if ((45 - count) > remainDays) {
+      // 剩余天数不够了，显示悲哀用语
+      hint = hintTable[3]
+    } else if ( count >= 45 ) {
+      // 完成跑操任务
+      hint = hintTable[4]
+    } else {
+      hint = hintTable[now % 2 + 1]
     }
     
+    return { count, detail:res.data.records, health, remainDays, hint}
 
-
-
-    // 剩余天数单独缓存，防止受跑操上游故障影响
-    let remainDays = await this.publicCache('remainDays', '1h', async () => {
-      let now = moment()
-      let beginOfTerm = this.term.currentTerm && this.term.currentTerm.isLong && moment(this.term.currentTerm.startDate)
-
-      return beginOfTerm ? (
-        Array(16 * 7).fill() // 生成当前学期每一天的下标数组
-          // 当前学期每一天的跑操结束时间戳
-          // 注意这里要克隆一次，不能在原对象上直接操作
-          .map((_, i) => beginOfTerm.clone().add(i, 'days').hour(7).minute(20))
-          // 去掉已经过去的，转换成星期，去掉双休日，剩下的天数
-          .filter(k => now < k).map(k => k.day()).filter(k => k >= 1 && k <= 5).length
-      ) : 0
-    })
-
-    return { count, detail, health, remainDays, hint: hintTable[0] }
   }
 }
