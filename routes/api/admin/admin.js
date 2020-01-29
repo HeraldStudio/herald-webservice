d2b3dd7bd859a637d88e796c2c0cef4860d6c1af
ZZJ <zhaozjseu@163.com>
2020-01-29 18:37:33 +0800

    完成/api/admin/admin路由

--- routes/api/admin/admin.js
+++ routes/api/admin/admin.js
@@ -1,4 +1,4 @@
-let mongodb = require('../../../database/mongodb')
+const monent = require('moment')
 exports.route = {
 
   /**
@@ -7,98 +7,227 @@ exports.route = {
   * 带 domain 参数表示查询指定域下的管理员；不带 domain 参数表示查询自己的管理员身份
   * 
   */
-  async get({ domain }) {
+  async get({ domain = '' }) {
+    if (!this.user.isLogin) {
+      throw 403
+    }
+    if (domain){
+      await this.hasPermission(domain)
+      let adminList = await this.db.execute(`
+      SELECT p.PERMISSION ,a.CARDNUM ,a.PHONENUM ,a.ACCESS_LEVEL, a.REAL_NAME 
+      FROM TOMMY.H_ADMIN a, TOMMY.H_ADMIN_PERMISSION p 
+      WHERE a.CARDNUM = p.CARDNUM AND p.PERMISSION = :permission`
+      ,{ 
+        permission: domain
+      })
+      let res =[]
 
-    // 这个里面的东西要重新写了。
+      //整理数据格式
+      const fieldName = adminList.metaData.map(item => {
+        if (item.name.split('_').length === 1) {
+          return item.name.toLowerCase()
+        } else {
+          return item.name.split('_')[0].toLowerCase() +
+            (item.name.split('_')[1].charAt(0).toUpperCase() + item.name.split('_')[1].slice(1).toLowerCase())
+        }
+      })
+      const data = adminList.rows
+      data.forEach(oneData => {
+        let tempData = {}
+        oneData.forEach((item, index) => {
+          tempData[fieldName[index]] = item
+        })
+        res.push(tempData)
+      })
+
+      return res
+
+    } else{
+      return await this.listPermission()
+    }
 
   },
 
   /**
   * api {POST} /api/admin/admin
-  * 任命管理员
-  * apiParam { domain, admin: { name, cardnum, phone } }
+  * 添加管理员权限
+  * @apiParam { domain, admin: { name, cardnum, phone } }
   */
   async post({ domain, admin }) {
-    let adminCollection = await mongodb('herald_admin')
-    //let domainCollection = await mongodb('herald_admin_domain')
-    let { name, cardnum, phone } = admin
-
-    // 只允许同域任命
-    if (!this.admin[domain]) {
+    const now = monent()
+    if (!this.user.isLogin) {
       throw 403
     }
+    
+    await this.hasPermission(domain)
+    
+    // 获取管理员权限等级
+    let adminLevel = await this.db.execute(`
+    SELECT ACCESS_LEVEL FROM TOMMY.H_ADMIN WHERE CARDNUM = :cardnum`,
+    {
+      cardnum: this.user.cardnum
+    })
+    adminLevel = adminLevel.rows[0][0]
 
-    //let has = await db.admin.find({ cardnum, domain }, 1)
-    let has = await adminCollection.findOne({ cardnum, domain })
-    if (has) {
-      throw '管理员已存在'
+    // 确定新管理员权限等级
+    let level = await this.db.execute(`
+    SELECT ACCESS_LEVEL FROM TOMMY.H_ADMIN WHERE CARDNUM = :cardnum`,
+    {
+      cardnum: admin.cardnum
+    })
+    const isHasLevel = level.rows.length !== 0
+    if(!isHasLevel){
+      // 没有新管理员的权限等级就新建一个
+      await this.db.execute(`INSERT INTO TOMMY.H_ADMIN 
+      (CARDNUM, PHONENUM, ACCESS_LEVEL, LAST_INVOKED_TIME, REAL_NAME)
+      VALUES (:cardnum, :phonenum, :accessLevel, :lastInvokedTime, :realName)
+      `,
+      {
+        cardnum: admin.cardnum,
+        phonenum: admin.phone,
+        accessLevel: ++adminLevel,
+        lastInvokedTime: now.toDate(),
+        realName: admin.name
+      })
+    }else{
+      // 判断权限等级是否满足
+      if(adminLevel >= level.rows[0][0]){
+        throw "权限等级不足"
+      }
+      // 有的话就更新一下时间
+      await this.db.execute(`
+      UPDATE TOMMY.H_ADMIN SET LAST_INVOKED_TIME =: lastInvokedTime WHERE CARDNUM = :cardnum
+      `,{
+        lastInvokedTime: now.toDate(),
+        cardnum: admin.cardnum
+      })
     }
 
-    let now = +moment()
-    // await db.admin.insert({
-    //   cardnum, name, phone, domain,
-    //   level: this.admin[domain].level + 1,
-    //   authorized: now,
-    //   lastUsed: now
-    // })
-    
-    await adminCollection.insertOne({
-      cardnum, name, phone, domain,
-      level: this.admin[domain].level + 1,
-      authorized: now,
-      lastUsed: now
+    // 添加权限
+    // 判断该管理员是否有该权限
+    let isHasPermission = await this.db.execute(`
+    SELECT COUNT(*) FROM TOMMY.H_ADMIN_PERMISSION 
+    WHERE CARDNUM = :cardnum AND PERMISSION = :permission
+    `,
+    {
+      cardnum: admin.cardnum,
+      permission: domain
     })
+    if(isHasPermission.rows[0][0]){
+      throw '该管理员已有该权限'
+    }
+    await this.db.execute(`
+    INSERT INTO TOMMY.H_ADMIN_PERMISSION (CARDNUM,PERMISSION) VALUES (:cardnum, :permission)
+    `,
+    {
+      cardnum: admin.cardnum,
+      permission: domain
+    })
+
     return 'OK'
   },
 
   /**
   * api {PUT} /api/admin/admin
   * 修改管理员信息
-  * apiParam { domain, admin }
+  * @apiParam { admin: { name, cardnum, phone } }
   */
-  async put({ domain, admin }) {
-
-    let adminCollection = await mongodb('herald_admin')
-
-    // 只允许同域任免
-    if (!this.admin[domain]) {
+  async put({ admin }) {
+    const now = monent()
+    if (!this.user.isLogin) {
       throw 403
     }
 
-    let has = await adminCollection.findOne({ cardnum: admin.cardnum, domain })
-    if (!has) {
-      throw '管理员不存在'
-    } else if (has.level <= this.admin[domain].level) {
-      throw '无法修改同级或高级管理员'
+    // 获取当前用户的权限等级
+    let adminLevel = await this.db.execute(`
+    SELECT ACCESS_LEVEL FROM TOMMY.H_ADMIN WHERE CARDNUM = :cardnum`,
+    {
+      cardnum: this.user.cardnum
+    })
+    adminLevel = adminLevel.rows[0][0]
+
+    // 获取被修改管理员的权限等级
+    let level = await this.db.execute(`
+    SELECT ACCESS_LEVEL FROM TOMMY.H_ADMIN WHERE CARDNUM = :cardnum`,
+    {
+      cardnum: admin.cardnum
+    })
+    level = level.rows[0][0]
+
+    if(adminLevel >= level){
+      throw '权限等级不足'
     }
 
-    //await db.admin.update({ cardnum: admin.cardnum, domain }, admin)
-    await adminCollection.updateOne({ cardnum: admin.cardnum, domain }, { $set: admin })
+    await this.db.execute(`
+    UPDATE TOMMY.H_ADMIN
+      SET PHONENUM = :phonenum, REAL_NAME = :name, LAST_INVOKED_TIME = :lastInvokedTime
+      WHERE CARDNUM= :cardnum`,
+    {
+      cardnum: admin.cardnum,
+      phonenum: admin.phone,
+      name: admin.name,
+      lastInvokedTime: now.toDate(),
+    })
+
     return 'OK'
   },
 
   /**
   * api {DELETE} /api/admin/admin
-  * 删除管理员
-  * apiParam { domain, cardnum }
+  * 删除管理员权限
+  * @apiParam { domain, cardnum }
   */
-  async delete({ domain, cardnum }) {
-    let adminCollection = await mongodb('herald_admin')
-    // 只允许同域任免
-    if (!this.admin[domain]) {
+  async delete({ domain , cardnum }) {
+    const now = monent()
+    if (!this.user.isLogin) {
       throw 403
     }
 
-    //let has = await db.admin.find({ cardnum, domain }, 1)
-    let has = await adminCollection.findOne({ cardnum, domain })
-    if (!has) {
-      throw '管理员不存在'
-    } else if (has.level <= this.admin[domain].level) {
-      throw '无法删除同级或高级管理员'
+    // 指明删除的权限
+    if (!domain) {
+      throw '未指定权限范围'
     }
 
-    //await db.admin.remove({ cardnum, domain })
-    await adminCollection.deleteMany({ cardnum, domain })
+    // 确定当前用户的权限
+    await this.hasPermission(domain)
+
+    // 获取管理员权限等级
+    let adminLevel = await this.db.execute(`
+    SELECT ACCESS_LEVEL FROM TOMMY.H_ADMIN WHERE CARDNUM = :cardnum`,
+    {
+      cardnum: this.user.cardnum
+    })
+    adminLevel = adminLevel.rows[0][0]
+
+    // 确定删除管理员权限等级
+    let level = await this.db.execute(`
+    SELECT ACCESS_LEVEL FROM TOMMY.H_ADMIN WHERE CARDNUM = :cardnum`,
+    {
+      cardnum
+    })
+    level = level.rows[0][0]
+
+    if( adminLevel>= level ){
+      throw '权限等级不足'
+    }
+
+    // 删除权限
+    await this.db.execute(`
+    DELETE FROM TOMMY.H_ADMIN_PERMISSION 
+    WHERE CARDNUM = :cardnum AND PERMISSION = :permission`, 
+    { 
+      cardnum,
+      permission:domain
+    })
+    // 修改时间
+    await this.db.execute(`
+    UPDATE TOMMY.H_ADMIN SET LAST_INVOKED_TIME = :lastInvokedTime
+    WHERE CARDNUM= :cardnum`,
+    {
+      cardnum,
+      lastInvokedTime: now.toDate(),
+    })
+
     return 'OK'
   }
 }
