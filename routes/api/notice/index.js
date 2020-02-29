cd731bcfcbdebed037e2f593667c7790caec66e9
R-shrug <656598766@qq.com>
2020-02-29 21:16:39 +0800

    改回去！

--- routes/api/notice/index.js
+++ routes/api/notice/index.js
@@ -234,9 +234,6 @@ exports.route = {
         }
         let { data } = await this.get(url)
         let $ = cheerio.load(data)
-        if(this.user.platform === 'app' || this.user.platform === 'app-debug'){
-          return $(typeObj.contentSelector || "[frag='窗口3']").html()
-        }
         let ret = new Europa({
           absolute: true,
           baseUri: typeObj.baseUrl,
