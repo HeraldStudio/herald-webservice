e67f85c26cffcaf2fb3937254a76aee0cf3c0259
R-shrug <656598766@qq.com>
2020-03-01 23:01:58 +0800

    添加获取更新信息的接口

--- routes/api/version.js
+++ routes/api/version.js
@@ -1,7 +1,30 @@
-const { config } = require('../../app')
-
 exports.route = {
-  get () {
-    return config.version
+  // 获取更新信息
+  async get({ version }) {
+    let record = await this.db.execute(`
+    SELECT VERSION_NUM, CREATEDTIME, DESCRIPTION, DOWNLOAD_URL
+    FROM H_VERSION
+    WHERE CREATEDTIME IN (
+      SELECT MAX(CREATEDTIME)
+      FROM H_VERSION
+    )
+  `)
+    record = record.rows.map(Element => {
+      let [version, createdTime, desc, downloadUrl] = Element
+      return { version, createdTime, desc, downloadUrl }
+    })[0]
+    if (version !== record.version) {
+      return {
+        isLatest: false,                              // 是否最新版本
+        version: record.version,                      // 最新版本号
+        desc: record.desc,                            // 版本描述
+        updateTime: record.createdTime,               // 版本更新时间
+        downloadUrl: record.downloadUrl               // 下载地址
+      }
+    }else{
+      return {
+        isLatest: true
+      }
+    }
   }
 }
