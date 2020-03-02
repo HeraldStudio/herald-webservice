c69ce932d7cb0d9ef0fca9ef3a75462e3d91dfbb
R-shrug <656598766@qq.com>
2020-03-02 10:58:45 +0800

    添加版本号格式检验

--- routes/api/version.js
+++ routes/api/version.js
@@ -1,7 +1,12 @@
 exports.route = {
   // 获取更新信息
   async get({ version }) {
+    // 验证是否符合版本号格式
+    if(!(/^\d+\.\d+\.\d+$/.test(version))){
+      throw '参数不符合规范'
+    }
     let { token, cardnum, platform } = this.user
+    // 获取最新版本
     let record = await this.db.execute(`
     SELECT VERSION_NUM, CREATEDTIME, DESCRIPTION, DOWNLOAD_URL
     FROM H_VERSION
@@ -10,15 +15,17 @@ exports.route = {
       FROM H_VERSION
     )
   `)
+    // 用户数据统计
     await this.db.execute(`
     INSERT INTO
     H_USER_STATISTICS
-    (TOKEN_HASH, CARDNUM, PLATFORM)
-    VALUES (:token, :cardnum, :platform)
+    (TOKEN_HASH, CARDNUM, PLATFORM, VERSION)
+    VALUES (:token, :cardnum, :platform, :version)
     `,{
       token,
       cardnum,
-      platform
+      platform,
+      version
     })
     record = record.rows.map(Element => {
       let [version, createdTime, desc, downloadUrl] = Element
