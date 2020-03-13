3e1a53c1b87fc53f68d5a7d7c1e5f7809929f51d
R-shrug <656598766@qq.com>
2020-03-13 10:47:34 +0800

    修改minio接口

--- routes/api/minio.js
+++ routes/api/minio.js
@@ -1,15 +1,26 @@
 exports.route = {
   async get({ annex }) {
-    // 获取minio 生成的临时下载链接，有效期一小时
-    let url = await new Promise((resolve, reject) => {
-      this.minio.presignedGetObject('annex', annex, 60 * 60, async (err, presignedUrl) => {
-        if (err) {
-          reject(err)
-          throw '获取文件失败'
-        }
-        resolve(presignedUrl)
+    let url
+    try {
+      url = await new Promise((resolve, reject) => {
+        this.minio.presignedGetObject('annex', annex, 60 * 60, async (err, presignedUrl) => {
+          if (err) {
+            console.log(err)
+            reject(err)
+            throw '下载文件失败'
+          }
+          resolve(presignedUrl)
+        })
       })
-    })
-    return url
+    } catch (err) {
+      throw '下载文件失败'
+    }
+    let res = await this.get(url)
+    let filename = annex.split('/').pop()
+    this.set('Content-Disposition',`attachment;filename=${encodeURI(filename)}`)
+    return {
+      res,
+      filename
+    }
   }
 }
