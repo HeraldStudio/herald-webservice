06e2de10c68b2fe90dcee3c6e2e78bbb84ec179f
R-shrug <656598766@qq.com>
2020-04-07 11:28:53 +0800

    修改下sql

--- routes/api/exam.js
+++ routes/api/exam.js
@@ -16,33 +16,30 @@ exports.route = {
     let response = await this.userCache('10m+', async () => {
       // 获取考试数据
       let record = await this.db.execute(`
-      select h.XNXQDM, MC, KCM, T_KW_KSPC.KSMC, h.XM, KSSJMS, JASMC, KSSC
-      from T_KW_KSPC,(
-        select  g.XNXQDM,g.MC,T_KC_KCB.KCM,T_JZG_JBXX.XM,g.KSSJMS,JASMC, KSSC, g. KSDM
-        from T_JZG_JBXX,T_KC_KCB,(
-          select   T_RW_JSB.JSH,f.MC, f.JASMC, f.KSRWID, f.KSSC, f.KSSJMS, f.XNXQDM, f.KCH, f.JXBID, f.KSDM
-          from T_RW_JSB,(
-            select T_ZXBZ_XXXQ.MC,e.JASMC, KSRWID, KSSC, KSSJMS, XNXQDM, KCH, JXBID, KSDM
-            from T_ZXBZ_XXXQ,(
-              select  d.JASMC, d.KSRWID, d.KSSC, d.KSSJMS, d.XNXQDM,T_KW_KSRW.KCH,XXXQDM,JXBID,KSDM
-              from T_KW_KSRW,(
-                select T_JAS_JBXX.JASMC,c.KSRWID, KSSC, KSSJMS, XNXQDM
-                from T_JAS_JBXX,(
-                  select T_KW_KSAPDD.JASDM,b.KSRWID,b.KSSC,b.KSSJMS,b.XNXQDM
-                  from T_KW_KSAPDD,(
-                    select T_KW_KSAPSJ.XNXQDM,T_KW_KSAPSJ.KSSJMS,T_KW_KSAPSJ.KSSC,T_KW_KSAPSJ.KSRWID,a.KSAPBH
-                    from T_KW_KSAPSJ,(
-                      select T_KW_KSAPXS.KSRWID,T_KW_KSAPXS.KSAPBH
-                      from T_KW_KSAPXS
-                      where T_KW_KSAPXS.xh=:cardnum)a
-                    where a.KSRWID=T_KW_KSAPSJ.KSRWID)b
-                  where T_KW_KSAPDD.KSRWID=b.KSRWID and T_KW_KSAPDD.KSAPBH=b.KSAPBH)c
-                where T_JAS_JBXX.JASDM=c.JASDM)d
-              where d.KSRWID=T_KW_KSRW.KSRWID)e
-            where T_ZXBZ_XXXQ.DM=e.XXXQDM)f
-          where T_RW_JSB.JXBID=f.JXBID and T_RW_JSB.KCH=f.KCH)g
-        where T_JZG_JBXX.ZGH=g.JSH and g.KCH=T_KC_KCB.KCH)h
-      where T_KW_KSPC.KSDM = h.KSDM
+      select T_KW_KSAPSJ.XNXQDM,T_ZXBZ_XXXQ.MC,T_KC_KCB.KCM,T_KW_KSPC.KSMC,T_JZG_JBXX.XM,T_KW_KSAPSJ.KSSJMS,T_JAS_JBXX.JASMC,T_KW_KSAPSJ.KSSC
+      from (
+        select *
+        from T_KW_KSAPXS
+        where T_KW_KSAPXS.xh=:cardnum
+      )a
+      left join T_KW_KSAPSJ
+      on a.ksrwid = T_KW_KSAPSJ.ksrwid
+      left join T_KW_KSAPDD
+      on a.ksrwid = T_KW_KSAPDD.ksrwid and a.ksapbh = T_KW_KSAPDD.ksapbh
+      left join T_JAS_JBXX
+      on t_kw_ksapdd.jasdm = T_JAS_JBXX.jasdm
+      left join T_KW_KSRW
+      on a.ksrwid = T_KW_KSRW.ksrwid
+      left join T_ZXBZ_XXXQ
+      on T_ZXBZ_XXXQ.dm = T_KW_KSRW.XXXQDM
+      left join T_RW_JSB
+      on T_RW_JSB.jxbid = t_kw_ksrw.jxbid and T_RW_JSB.kch = t_kw_ksrw.kch
+      left join t_jzg_jbxx
+      on t_jzg_jbxx.zgh = t_rw_jsb.jsh
+      left join T_KC_KCB
+      on t_kw_ksrw.kch = t_kc_kcb.kch
+      left join T_KW_KSPC
+      on T_KW_KSPC.ksdm = t_kw_ksrw.ksdm
       `, [cardnum])
       let result = record.rows.map(Element => {
         let [semester, campus, courseName, courseType, teacherName, time, location, duration] = Element
