bb0f1ff35cefc6127c4141e411887be290cb4529
root <213183580@seu.edu.cn>
2020-08-11 20:29:33 +0800

    功能待测试字段

--- routes/api/oauth/service.js
+++ routes/api/oauth/service.js
@@ -1,16 +1,26 @@
 exports.route = {
-  async get() {
-    let record = await this.db.execute(`
-    SELECT *
-    FROM H_OAUTH_SERVICE
-    `)
+  async get({ underTest = 0 }) {
+    let record
+    if (underTest) {
+      record = await this.db.execute(`
+      SELECT *
+      FROM H_OAUTH_SERVICE
+      WHERE UNDERTEST = 0
+      `)
+    } else {
+      record = await this.db.execute(`
+      SELECT *
+      FROM H_OAUTH_SERVICE
+      WHERE UNDERTEST = 0
+      `)
+    }
     let result = {}
     record.rows.map(Element => {
       let [name, url, state, icon, type] = Element
       if (result[type] === undefined) {
         result[type] = []
       }
-      result[type].push({name, url, state, icon})
+      result[type].push({ name, url, state, icon })
     })
     return result
   }
