ef78a2a4384425533d8ea2287e84220894438c29
R-shrug <213183580@seu.edu.cn>
2020-08-28 22:50:30 +0800

    更加优雅

--- middleware/version.js
+++ middleware/version.js
@@ -1,18 +1,20 @@
 module.exports = async (ctx, next) => {
-  if (ctx.path === '/app-download/android') {
-    let record = await ctx.db.execute(`
-    SELECT DOWNLOAD_URL
-    FROM H_VERSION
-    WHERE CREATEDTIME IN (
-      SELECT MAX(CREATEDTIME)
+  if (ctx.path === '/app-download') {
+    if (this.request.headers['user-agent'].toLowerCase().includes('ios')) {
+      ctx.redirect('https://apps.apple.com/cn/app/id1378941139')
+    } else {
+      let record = await ctx.db.execute(`
+      SELECT DOWNLOAD_URL
       FROM H_VERSION
-      WHERE PLATFORM = 'app-android'
-    )AND PLATFORM = 'app-android'
-    `)
-    ctx.redirect(record.rows[0][0])
-  } else if (ctx.path === '/app-download/ios') {
-    ctx.redirect('https://apps.apple.com/cn/app/id1378941139')
-  } {
+      WHERE CREATEDTIME IN (
+        SELECT MAX(CREATEDTIME)
+        FROM H_VERSION
+        WHERE PLATFORM = 'app-android'
+      )AND PLATFORM = 'app-android'
+      `)
+      ctx.redirect(record.rows[0][0])
+    }
+  } else {
     await next()
   }
 }
