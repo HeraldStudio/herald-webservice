dcdf089e90de3fac1611b2f17b6118ba98e0c268
R-shrug <213183580@seu.edu.cn>
2020-08-29 00:25:47 +0800

    bugfix

---
+++ assets/wechatTip.js
@@ -0,0 +1,23 @@
+module.exports = `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>请在微信外打开此链接</title>
+</head>
+<style type="text/css">
+</style>
+
+<body>
+  <div style="width:100%; display:flex; flex-direction:column; align-items:center; justify-content:center;">
+    <p>
+      <img style="width:200px" src="./wechatTip.jpeg">
+    <p><b>麻烦点击右上角的 (···) 在浏览器里打开，求求你了 QWQ</b></p>
+    </p>
+  </div>
+</body>
+
+</html>
+`
--- middleware/version.js
+++ middleware/version.js
@@ -1,19 +1,24 @@
 module.exports = async (ctx, next) => {
   if (ctx.path === '/app-download') {
-    console.log(ctx.request.headers['user-agent'])
     if (ctx.request.headers['user-agent'].toLowerCase().includes('ios') || ctx.request.headers['user-agent'].toLowerCase().includes('mac')) {
       ctx.redirect('https://apps.apple.com/cn/app/id1378941139')
     } else {
-      let record = await ctx.db.execute(`
-      SELECT DOWNLOAD_URL
-      FROM H_VERSION
-      WHERE CREATEDTIME IN (
-        SELECT MAX(CREATEDTIME)
+      if (ctx.request.headers['user-agent'].toLowerCase().includes('micromessenger')) {
+        ctx.type = 'text/html;charset=utf-8'
+        ctx.body = require('../assets/wechatTip')
+        return
+      } else {
+        let record = await ctx.db.execute(`
+        SELECT DOWNLOAD_URL
         FROM H_VERSION
-        WHERE PLATFORM = 'app-android'
-      )AND PLATFORM = 'app-android'
-      `)
-      ctx.redirect(record.rows[0][0])
+        WHERE CREATEDTIME IN (
+          SELECT MAX(CREATEDTIME)
+          FROM H_VERSION
+          WHERE PLATFORM = 'app-android'
+        )AND PLATFORM = 'app-android'
+        `)
+        ctx.redirect(record.rows[0][0])
+      }
     }
   } else {
     await next()
