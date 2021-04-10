a5ef39dd1fd0dc1c287f70cf41ed9e5cb8b341f2
unconsolable <chenzhipeng2012@gmail.com>
2021-04-10 11:10:51 +0800

    增加 根据讲座ID获取讲座详细信息

---
+++ routes/api/lecture/admin/detail.js
@@ -0,0 +1,23 @@
+exports.route = {
+  /**
+  * @api {GET} /api/lecture/detail 根据讲座ID获取讲座详细信息
+  * @apiGroup lecture
+  */
+  async get({id}) {
+    if (!(await this.hasPermission('lecturerecord'))) {
+      throw 403
+    }
+    if (!id) {
+      throw "参数不全"
+    }
+    let records = await this.db.execute(`
+      SELECT NAME, DATESTR, LOCATION, URL
+      FROM H_LECTURE_HISTORY
+      WHERE ID = :id
+    `, { id })
+    return records.rows.map(result => {
+      const [name, dateStr, location, url] = result
+      return {name, dateStr, location, url}
+    })
+  }
+}
