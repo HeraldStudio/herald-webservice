3c01b57d9bdd08b7674f6fda00aa5310c6efbeb7
unconsolable <chenzhipeng2012@gmail.com>
2021-04-17 14:39:12 +0800

    修复lecture-history中间件中时间判断异常

--- middleware/lecture-history.js
+++ middleware/lecture-history.js
@@ -2,9 +2,10 @@ let lectureMap = {}
 let lastInvokeTime = 0
 
 module.exports = async (ctx, next) => {
-  let now = moment().unix()
+  let now = moment()
   // 每24小时更新一次
   if (now - lastInvokeTime >= 24 * 60 * 60 * 1000) {
+    lastInvokeTime = now
     lectureMap = {}
     let record = await ctx.db.execute(`
       SELECT NAME, DATESTR, LOCATION, URL
@@ -20,7 +21,6 @@ module.exports = async (ctx, next) => {
       const [name, dateStr, location, url] = r
       lectureMap[r[1]][r[2]].push({name, dateStr, location, url})
     })
-    lastInvokeTime = moment().unix()
   }
   ctx.lectureMap = lectureMap
   await next()
