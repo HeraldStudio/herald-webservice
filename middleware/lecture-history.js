bf215c5705b5a9c2dbb479609f134254fe1a8f24
unconsolable <chenzhipeng2012@gmail.com>
2021-04-17 14:14:07 +0800

    修复因lectureMap重复计数引起的去重失效

--- middleware/lecture-history.js
+++ middleware/lecture-history.js
@@ -5,6 +5,7 @@ module.exports = async (ctx, next) => {
   let now = moment().unix()
   // 每24小时更新一次
   if (now - lastInvokeTime >= 24 * 60 * 60 * 1000) {
+    lectureMap = {}
     let record = await ctx.db.execute(`
       SELECT NAME, DATESTR, LOCATION, URL
       FROM H_LECTURE_HISTORY
